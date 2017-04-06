2a6878da2ea7624f8df1bb4b80f4294769e8d9d4
Steven Cochrane <s.cochrane@kainos.com>
2017-04-06 14:06:05 +0100

    243: removed incorrect POST request, updated GET request to take in date parameter vs year param

--- app/routes/offender-manager-utilisation.js
+++ app/routes/offender-manager-utilisation.js
@@ -7,12 +7,4 @@ module.exports = function (router) {
       utilisation: getUtilisationTable(ORG_UNIT_TYPE.OFFENDER_MANAGER, req.params.id, req.query.date)
     })
   })
-
-  router.post(`/caseload-utilisation/${ORG_UNIT_TYPE.OFFENDER_MANAGER}/:userId/:year/`, function (req, res, next) {
-    date = req.body.date
-    splitDate = date.split('-', 3)
-    return res.render('utilisation', {
-      utilisation: getUtilisationTable(ORG_UNIT_TYPE.OFFENDER_MANAGER, req.params.userId, splitDate[0])
-    })
-  })
 }
