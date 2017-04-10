e320b632ac0a4811a2ca68c4085c212748a26ea0
Steven Cochrane <s.cochrane@kainos.com>
2017-04-10 12:06:33 +0100

    WMT-243: reading in from/to date from view in standard gov design approach.

--- app/routes/offender-manager-utilisation.js
+++ app/routes/offender-manager-utilisation.js
@@ -3,8 +3,21 @@ const ORG_UNIT_TYPE = require('../constants/organisation-unit-type-enum')
 
 module.exports = function (router) {
   router.get(`/caseload-utilisation/${ORG_UNIT_TYPE.OFFENDER_MANAGER}/:id/`, function (req, res, next) {
+
+    fromDate = [
+      req.query['utilisation-from-year'],
+      req.query['utilisation-from-month'],
+      req.query['utilisation-from-day']
+    ]
+
+    toDate = [
+      req.query['utilisation-to-year'],
+      req.query['utilisation-to-month'],
+      req.query['utilisation-to-day']
+    ]
+
     return res.render('utilisation', {
-      utilisation: getUtilisationTable(ORG_UNIT_TYPE.OFFENDER_MANAGER, req.params.id, req.query.date)
+      utilisation: getUtilisationTable(ORG_UNIT_TYPE.OFFENDER_MANAGER, req.params.id, fromDate, toDate)
     })
   })
 }
