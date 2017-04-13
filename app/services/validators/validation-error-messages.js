2c859f53bc2e9f28a4fe58650880af880f1bafae
Steven Cochrane <s.cochrane@kainos.com>
2017-04-13 16:12:42 +0100

    WMT-275: adding error message text and template for inclusion on input pages.

---
+++ app/services/validators/validation-error-messages.js
@@ -0,0 +1,5 @@
+module.exports = {
+  getIsRequired: function (displayName) { return `${displayName} is required` },
+  getInvalidDateFormatMessage: function (displayName) { return `${displayName} was invalid` },
+  getIsOlderThanMaxHistory: function (displayName, options) { return `Date range must be within the past ${options.years} years`},
+}
