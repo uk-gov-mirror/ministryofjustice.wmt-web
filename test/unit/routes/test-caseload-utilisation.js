885f0a426aa19bdd2cbce332ad4218433b9250f4
Steven Cochrane <s.cochrane@kainos.com>
2017-04-14 15:33:05 +0100

    WMT-275: updated offender manager utilisation route tests to cover independent day, month and year text based input

--- test/unit/routes/test-caseload-utilisation.js
+++ test/unit/routes/test-caseload-utilisation.js
@@ -9,6 +9,15 @@ const expect = require('chai').expect
 const OFFENDER_MANAGER_UTILISATION_URI = '/caseload-utilisation/offendermanager'
 const ID = 1
 const DATE = '2017-03-01T00:00:00Z'
+const UTILISATION_FROM_DAY = 'utilisation-from-day='
+const UTILISATION_FROM_MONTH = 'utilisation-from-month='
+const UTILISATION_FROM_YEAR = 'utilisation-from-year='
+
+const UTILISATION_TO_DAY = 'utilisation-to-day='
+const UTILISATION_TO_MONTH = 'utilisation-to-month='
+const UTILISATION_TO_YEAR = 'utilisation-to-year='
+
+
 const YEAR = 2016
 
 describe('/caseload-utilisation', function () {
@@ -28,13 +37,96 @@ describe('/caseload-utilisation', function () {
         .expect(200)
     })
 
-    it('should respond with a 200 when valid URI and date query is used', function () {
+    it('should respond with a 200 when valid URI and from/to date query is used', function () {
 
       return supertest(app)
-        .get(OFFENDER_MANAGER_UTILISATION_URI + "/" + ID + "?date=" + DATE)
+        .get(OFFENDER_MANAGER_UTILISATION_URI + "/" + ID + "?"
+        + UTILISATION_FROM_DAY + "01&"
+        + UTILISATION_FROM_MONTH + "01&"
+        + UTILISATION_FROM_YEAR + "2017&"
+        + UTILISATION_TO_DAY + "31&"
+        + UTILISATION_TO_MONTH + "03&"
+        + UTILISATION_TO_YEAR + "2017")
         .expect(200)
     })
 
+    it('should respond with a 400 when valid URI and invalid from day is used', function () {
+
+      return supertest(app)
+        .get(OFFENDER_MANAGER_UTILISATION_URI + "/" + ID + "?"
+        + UTILISATION_FROM_DAY + "33&"
+        + UTILISATION_FROM_MONTH + "01&"
+        + UTILISATION_FROM_YEAR + "2017&"
+        + UTILISATION_TO_DAY + "31&"
+        + UTILISATION_TO_MONTH + "31&"
+        + UTILISATION_TO_YEAR + "2017")
+        .expect(400)
+    })
+
+    it('should respond with a 400 when valid URI and invalid from month is used', function () {
+
+      return supertest(app)
+        .get(OFFENDER_MANAGER_UTILISATION_URI + "/" + ID + "?"
+        + UTILISATION_FROM_DAY + "01&"
+        + UTILISATION_FROM_MONTH + "jeff&"
+        + UTILISATION_FROM_YEAR + "2017&"
+        + UTILISATION_TO_DAY + "31&"
+        + UTILISATION_TO_MONTH + "31&"
+        + UTILISATION_TO_YEAR + "2017")
+        .expect(400)
+    })
+
+    it('should respond with a 400 when valid URI and invalid from year is used', function () {
+
+      return supertest(app)
+        .get(OFFENDER_MANAGER_UTILISATION_URI + "/" + ID + "?"
+        + UTILISATION_FROM_DAY + "01&"
+        + UTILISATION_FROM_MONTH + "01&"
+        + UTILISATION_FROM_YEAR + "&"
+        + UTILISATION_TO_DAY + "31&"
+        + UTILISATION_TO_MONTH + "31&"
+        + UTILISATION_TO_YEAR + "2017")
+        .expect(400)
+    })
+
+    it('should respond with a 400 when valid URI and invalid from day is used', function () {
+
+      return supertest(app)
+        .get(OFFENDER_MANAGER_UTILISATION_URI + "/" + ID + "?"
+        + UTILISATION_FROM_DAY + "01&"
+        + UTILISATION_FROM_MONTH + "01&"
+        + UTILISATION_FROM_YEAR + "2017&"
+        + UTILISATION_TO_DAY + "33&"
+        + UTILISATION_TO_MONTH + "31&"
+        + UTILISATION_TO_YEAR + "2017")
+        .expect(400)
+    })
+
+    it('should respond with a 400 when valid URI and invalid from month is used', function () {
+
+      return supertest(app)
+        .get(OFFENDER_MANAGER_UTILISATION_URI + "/" + ID + "?"
+        + UTILISATION_FROM_DAY + "01&"
+        + UTILISATION_FROM_MONTH + "01&"
+        + UTILISATION_FROM_YEAR + "2017&"
+        + UTILISATION_TO_DAY + "31&"
+        + UTILISATION_TO_MONTH + "jeff&"
+        + UTILISATION_TO_YEAR + "2017")
+        .expect(400)
+    })
+
+    it('should respond with a 400 when valid URI and invalid to year is used', function () {
+
+      return supertest(app)
+        .get(OFFENDER_MANAGER_UTILISATION_URI + "/" + ID + "?"
+        + UTILISATION_FROM_DAY + "01&"
+        + UTILISATION_FROM_MONTH + "01&"
+        + UTILISATION_FROM_YEAR + "2017&"
+        + UTILISATION_TO_DAY + "31&"
+        + UTILISATION_TO_MONTH + "31&"
+        + UTILISATION_TO_YEAR + "")
+        .expect(400)
+    })
     // Tests do not use app.js where 404 handler is defined. Defaults to 500.
 
     it('should respond with a 500 when id is missing'
