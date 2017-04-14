7d0e77878266123a76295b6ebd5845096e6775cb
Steven Cochrane <s.cochrane@kainos.com>
2017-04-14 15:32:14 +0100

    WMT-275: updated offender manager utilisation route to include validation and to correct date input order

--- app/routes/offender-manager-utilisation.js
+++ app/routes/offender-manager-utilisation.js
@@ -2,41 +2,56 @@ const getUtilisationTable = require('../services/get-utilisation-table')
 const UtilisationDate = require('../services/domain/utilisation-date')
 const ORG_UNIT_TYPE = require('../constants/organisation-unit-type-enum')
 const dateFormatter = require('../services/date-formatter')
+const ValidationError = require('../services/errors/validation-error')
 
 module.exports = function (router) {
   router.get(`/caseload-utilisation/${ORG_UNIT_TYPE.OFFENDER_MANAGER}/:id/`, function (req, res, next) {
 
-    var fromUtilisationDate, toUtilisationDate
+    try {
+      var fromUtilisationDate, toUtilisationDate
 
-    if(Object.keys(req.query).length === 0) {
-      fromDate = dateFormatter.now().subtract(1, "years")
-      toDate = dateFormatter.now()
+      if(Object.keys(req.query).length === 0) {
+        fromDate = dateFormatter.now().subtract(1, "years")
+        toDate = dateFormatter.now()
 
-      fromUtilisationDate = new UtilisationDate(
-        fromDate.date(),
-        fromDate.month() +1,
-        fromDate.year()
-      )
-      toUtilisationDate = new UtilisationDate(
-        toDate.date(),
-        toDate.month() +1,
-        toDate.year()
-      )
-    } else {
-      fromUtilisationDate = new UtilisationDate(
-        req.query['utilisation-from-year'],
-        req.query['utilisation-from-month'],
-        req.query['utilisation-from-day']
-      )
-      toUtilisationDate = new UtilisationDate(
-        req.query['utilisation-to-year'],
-        req.query['utilisation-to-month'],
-        req.query['utilisation-to-day']
-      )
+        fromUtilisationDate = new UtilisationDate(
+          fromDate.date(),
+          fromDate.month() +1,
+          fromDate.year()
+        )
+        toUtilisationDate = new UtilisationDate(
+          toDate.date(),
+          toDate.month() +1,
+          toDate.year()
+        )
+      } else {
+
+        fromUtilisationDate = new UtilisationDate(
+          req.query['utilisation-from-day'],
+          req.query['utilisation-from-month'],
+          req.query['utilisation-from-year']
+        )
+        toUtilisationDate = new UtilisationDate(
+          req.query['utilisation-to-day'],
+          req.query['utilisation-to-month'],
+          req.query['utilisation-to-year']
+        )
+      }
+
+      return res.render('utilisation', {
+        utilisation: getUtilisationTable(ORG_UNIT_TYPE.OFFENDER_MANAGER, req.params.id, fromUtilisationDate, toUtilisationDate)
+      })
+
+    } catch (error) {
+      if (error instanceof ValidationError) {
+        return res.status(400).render('utilisation', {
+          errors: error.validationErrors,
+          // claimType: req.params.claimType,
+          // claimant: req.body
+        })
+      } else {
+        throw error
+      }
     }
-
-    return res.render('utilisation', {
-      utilisation: getUtilisationTable(ORG_UNIT_TYPE.OFFENDER_MANAGER, req.params.id, fromUtilisationDate, toUtilisationDate)
-    })
   })
 }
