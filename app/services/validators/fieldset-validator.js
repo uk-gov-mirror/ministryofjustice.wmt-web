39ee470a8ae3501d7abd9f8d9263ba1413bd0321
Steven Cochrane <s.cochrane@kainos.com>
2017-04-18 11:41:38 +0100

    WMT-275: Added fieldset validator and tests
    Added fieldset validator functions for validating caseload capacity dates
    Added fieldset validator tests to tie in with expected behaviour/scenarios so far

---
+++ app/services/validators/fieldset-validator.js
@@ -0,0 +1,56 @@
+const validator = require('./common-validator')
+const ERROR_MESSAGES = require('./validation-error-messages')
+
+class FieldsetValidator {
+
+  /**
+   * Build a validator for validating fieldsets (I.e. a group of fields).
+   * @param data An array of elements to validate as a set.
+   * @param fieldName The name of of the HTML element to link the error message to.
+   * @param errors An instance of the ErrorHandler class.
+   */
+  constructor (data, fieldName, errors) {
+    this.data = data || []
+    this.fieldName = fieldName
+    this.errors = errors
+  }
+
+  isRequired (specificMessage) {
+    var message = (!specificMessage) ? ERROR_MESSAGES.getIsRequired : specificMessage
+    var self = this
+    if (this.data instanceof Array) {
+      this.data.forEach(function (data) {
+        if (validator.isNullOrUndefined(data)) {
+          self.errors.add(self.fieldName, message)
+        }
+      })
+    }
+    return this
+  }
+
+  isValidDate (date) {
+    if (!validator.isValidDate(date)) {
+      this.errors.add(this.fieldName, ERROR_MESSAGES.getInvalidDateFormatMessage)
+    }
+    return this
+  }
+
+  isPastDate (date) {
+    if (!validator.isDateInThePast(date)) {
+      this.errors.add(this.fieldName, ERROR_MESSAGES.getPastDateMessage)
+    }
+    return this
+  }
+
+  isOlderThanMaxHistory (date) {
+    if (validator.isDateOlderThanMaxHistory(date)) {
+      //TODO: make constant for MAX_HISTORY
+      this.errors.add(this.fieldName, ERROR_MESSAGES.getIsOlderThanMaxHistory, { years: 6 })
+    }
+    return this
+  }
+}
+
+module.exports = function (data, fieldName, errors) {
+  return new FieldsetValidator(data, fieldName, errors)
+}
---
+++ test/unit/services/validators/test-fieldset-validator.js
@@ -0,0 +1,145 @@
+const expect = require('chai').expect
+const FieldsetValidator = require('../../../../app/services/validators/fieldset-validator')
+const ErrorHandler = require('../../../../app/services/validators/error-handler')
+const ERROR_MESSAGES = require('../../../../app/services/validators/validation-error-messages')
+const dateFormatter = require('../../../../app/services/date-formatter')
+
+describe('services/validators/fieldset-validator', function () {
+  const VALID_DATA_ITEM_1 = 'data 1'
+  const VALID_DATA_ITEM_2 = 'data 2'
+  const INVALID_DATA_ITEM_1 = ''
+  const DATA = [
+    VALID_DATA_ITEM_1,
+    VALID_DATA_ITEM_2,
+    INVALID_DATA_ITEM_1
+  ]
+  const FIELD_NAME = 'field name'
+  const ERROR_HANDLER = ErrorHandler()
+
+  beforeEach(function () {
+    this.error = ErrorHandler()
+    this.fieldsetValidator = FieldsetValidator(DATA, FIELD_NAME, this.error)
+  })
+
+  describe('isRequired', function () {
+    it('should return false if data is null', function () {
+      FieldsetValidator(null, FIELD_NAME, ERROR_HANDLER)
+        .isRequired()
+      var errors = ERROR_HANDLER.get()
+      expect(errors).to.be.equal(false)
+    })
+
+    it('should return false if data is undefined', function () {
+      FieldsetValidator(undefined, FIELD_NAME, ERROR_HANDLER)
+        .isRequired()
+      var errors = ERROR_HANDLER.get()
+      expect(errors).to.be.equal(false)
+    })
+
+    it('should return false if data is an object', function () {
+      FieldsetValidator({}, FIELD_NAME, ERROR_HANDLER)
+        .isRequired()
+      var errors = ERROR_HANDLER.get()
+      expect(errors).to.be.equal(false)
+    })
+
+    it('should return an error object if passed an array containing invalid data', function () {
+      FieldsetValidator(DATA, FIELD_NAME, ERROR_HANDLER)
+        .isRequired()
+      var errors = ERROR_HANDLER.get()
+      expect(errors).to.have.property(FIELD_NAME)
+    })
+
+    it('should return the fieldsetValidator after being called to allow function chaining.', function () {
+      var result = this.fieldsetValidator.isRequired()
+      expect(result).to.be.equal(this.fieldsetValidator)
+    })
+  })
+
+  describe('isValidDate', function () {
+    it('should return error object if data is null', function () {
+      this.fieldsetValidator.isValidDate(null)
+      var errors = this.error.get()
+      expect(errors).to.have.property(FIELD_NAME)
+    })
+
+    it('should return error object if data is undefined', function () {
+      this.fieldsetValidator.isValidDate(undefined)
+      var errors = this.error.get()
+      expect(errors).to.have.property(FIELD_NAME)
+    })
+
+    it('should return error object if data is not a valid date object', function () {
+      this.fieldsetValidator.isValidDate({})
+      var errors = this.error.get()
+      expect(errors).to.have.property(FIELD_NAME)
+    })
+
+    it('should return false if the date given is valid', function () {
+      this.fieldsetValidator.isValidDate(dateFormatter.now().subtract(18, 'years'))
+      var errors = this.error.get()
+      expect(errors).to.equal(false)
+    })
+
+    it('should return the fieldsetValidator after being called to allow function chaining.', function () {
+      var result = this.fieldsetValidator.isValidDate(dateFormatter.now())
+      expect(result).to.be.equal(this.fieldsetValidator)
+    })
+  })
+
+  describe('isPastDate', function () {
+    const PAST_DATE = dateFormatter.now().subtract(1, 'day')
+    const FUTURE_DATE = dateFormatter.now().add(1, 'day')
+
+    it('should return error object if data is null', function () {
+      this.fieldsetValidator.isPastDate(null)
+      var errors = this.error.get()
+      expect(errors).to.have.property(FIELD_NAME)
+    })
+
+    it('should return error object if data is undefined', function () {
+      this.fieldsetValidator.isPastDate(undefined)
+      var errors = this.error.get()
+      expect(errors).to.have.property(FIELD_NAME)
+    })
+
+    it('should return error object if data is not a valid date object', function () {
+      this.fieldsetValidator.isPastDate({})
+      var errors = this.error.get()
+      expect(errors).to.have.property(FIELD_NAME)
+    })
+
+    it('should return error object if the date given is in the future', function () {
+      this.fieldsetValidator.isPastDate(FUTURE_DATE)
+      var errors = this.error.get()
+      expect(errors).to.have.property(FIELD_NAME)
+    })
+
+    it('should return false if the date given is in the past', function () {
+      this.fieldsetValidator.isPastDate(PAST_DATE)
+      var errors = this.error.get()
+      expect(errors).to.equal(false)
+    })
+
+    it('should return the fieldsetValidator after being called to allow function chaining.', function () {
+      var result = this.fieldsetValidator.isPastDate(dateFormatter.now())
+      expect(result).to.be.equal(this.fieldsetValidator)
+    })
+  })
+
+  describe('isOlderThanMaxHistory', function () {
+    const MAX_HISTORY = 6
+    const GREATER_THAN_MAX_HISTORY = dateFormatter.now().subtract(MAX_HISTORY, 'years')
+
+    it(`should return error object if the date is more than ${MAX_HISTORY} years ago`, function () {
+      this.fieldsetValidator.isOlderThanMaxHistory(GREATER_THAN_MAX_HISTORY)
+      var errors = this.error.get()
+      expect(errors).to.have.property(FIELD_NAME)
+    })
+
+    it('should return the fieldsetValidator after being called to allow function chaining.', function () {
+      var result = this.fieldsetValidator.isOlderThanMaxHistory(GREATER_THAN_MAX_HISTORY)
+      expect(result).to.be.equal(this.fieldsetValidator)
+    })
+  })
+})
