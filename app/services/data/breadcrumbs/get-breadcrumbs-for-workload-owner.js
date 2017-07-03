8d1c356a3eed831677cea8ed18d44c63b1f59056
Andrew Wright <a.wright@kainos.com>
2017-07-03 13:19:24 +0100

    424: Add breadcrumb object for navigation

---
+++ app/services/data/breadcrumbs/get-breadcrumbs-for-workload-owner.js
@@ -0,0 +1,23 @@
+const config = require('../../../knexfile').web
+const knex = require('knex')(config)
+
+module.exports = function (workloadOwnerId) {
+    return knex('workload_owner')
+        .first('team_id',
+                'team.description as team_name',
+                'ldu_id',
+                'ldu.description as ldu_name',
+                'region_id',
+                'region.description as region_name')
+        .join('team', 'workload_owner.team_id', '=', 'team.id')
+        .join('ldu', 'team.ldu_id', '=', 'ldu.id')
+        .join('region', 'ldu.region_id', '=', 'region.id')
+        .where('workload_owner.id', workloadOwnerId)
+        .then((result) => {
+            return {
+                region: {id: result.region_id, name: result.region_name},
+                ldu: {id: result.ldu_id, name: result.ldu_name},
+                team: {id: result.team_id, name: result.team_name},
+            }
+        })
+}
