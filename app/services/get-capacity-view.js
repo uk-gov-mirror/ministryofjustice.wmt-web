fd9591cb1c303f5f29bbee5a827be7c1d4669097
Caoimhe Boyle <c.boyle@kainos.com>
2017-07-07 15:58:53 +0100

    424: Move tree build trigger from get-capactity-view to app.js
    Organisational Hierarchy tree built as part of app.js, not when view is called

--- app/services/get-capacity-view.js
+++ app/services/get-capacity-view.js
@@ -4,25 +4,19 @@ const getWorkloadReports = require('../services/data/get-workload-report-views')
 const routeType = require('../constants/organisation-unit')
 const tableCreator = require('../services/helpers/table-creator')
 
-// TODO Don't do this here
-const getOrganisationalHierarchyTree = require('./organisational-hierarchy-tree')
-
 module.exports = function (id, capacityDateRange, organisationLevel) {
-  // TODO Move building of OH tree out
-  return getOrganisationalHierarchyTree.build().then(function () {
-    var breadcrumbs = getBreadcrumbs(id, organisationLevel)
-    var result = {}
-    var workloadReportsPromise
-    organisationLevel === routeType.OFFENDER_MANAGER.name
+  var breadcrumbs = getBreadcrumbs(id, organisationLevel)
+  var result = {}
+  var workloadReportsPromise
+  organisationLevel === routeType.OFFENDER_MANAGER.name
     ? workloadReportsPromise = getIndividualWorkloadReports(id, capacityDateRange.capacityFromDate.toISOString(), capacityDateRange.capacityToDate.toISOString())
     : workloadReportsPromise = getWorkloadReports(id, capacityDateRange.capacityFromDate.toISOString(), capacityDateRange.capacityToDate.toISOString(), organisationLevel)
 
-    result.breadcrumbs = breadcrumbs
+  result.breadcrumbs = breadcrumbs
 
-    return workloadReportsPromise.then(function (results) {
-      result.capacityTable = tableCreator.createCapacityTable(id, 'TABLE TYPE GOES HERE', capacityDateRange, results)
-      result.title = organisationLevel + ' Capacity'
-      return result
-    })
+  return workloadReportsPromise.then(function (results) {
+    result.capacityTable = tableCreator.createCapacityTable(id, 'TABLE TYPE GOES HERE', capacityDateRange, results)
+    result.title = organisationLevel + ' Capacity'
+    return result
   })
 }
