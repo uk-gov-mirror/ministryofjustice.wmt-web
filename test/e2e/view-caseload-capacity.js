2c57f83e0f6ac3eb78541955a972516490002cad
Damien Coney <d.coney@kainos.com>
2017-07-13 10:25:25 +0100

    334: Adding caseload-progress selenium tests.

--- test/e2e/view-caseload-capacity.js
+++ test/e2e/view-caseload-capacity.js
@@ -2,49 +2,48 @@ const expect = require('chai').expect
 
 const workloadCapacityHelper = require('../helpers/data/workload-capacity-helper')
 
-var inserts = []
+var workloadOwnerIds = []
 
 describe('View your caseload capacity flow', () => {
   before(function () {
     return workloadCapacityHelper.selectIdsForWorkloadOwner()
-      .then(function (builtInserts) {
-        inserts = builtInserts
+      .then(function (results) {
+        workloadOwnerIds = results
       })
   })
 
   it('should navigate to the workload owner caseload capacity screen', () => {
-    return browser.url('/offender-manager/' + inserts.filter((item) => item.table === 'workload_owner')[0].id + '/caseload-capacity')
+    return browser.url('/offender-manager/' + workloadOwnerIds.filter((item) => item.table === 'workload_owner')[0].id + '/caseload-capacity')
       .waitForExist('.plot-container.plotly')
-      .waitForExist('.heading-large')
-      .getValue('.heading-large', function (title) {
-        expect(title).to.equal('Offender Manager Capacity')
+      .waitForExist('.heading-secondary')
+      .getValue('.heading-secondary', function (title) {
+        expect(title).to.contains('Offender Manager Capacity')
       })
   })
 
   it('should navigate to the ldu caseload capacity screen', () => {
-    return browser.url('/ldu/' + inserts.filter((item) => item.table === 'ldu')[0].id + '/caseload-capacity')
+    return browser.url('/ldu/' + workloadOwnerIds.filter((item) => item.table === 'ldu')[0].id + '/caseload-capacity')
       .waitForExist('.plot-container.plotly')
-      .waitForExist('.heading-large')
-      .getValue('.heading-large', function (title) {
-        console.log(title)
-        expect(title).to.equal('Ldu Capacity')
+      .waitForExist('.heading-secondary')
+      .getValue('.heading-secondary', function (title) {
+        expect(title).to.contains('Ldu Capacity')
       })
   })
 
   it('should navigate to the region caseload capacity screen', () => {
-    return browser.url('/region/' + inserts.filter((item) => item.table === 'region')[0].id + '/caseload-capacity')
+    return browser.url('/region/' + workloadOwnerIds.filter((item) => item.table === 'region')[0].id + '/caseload-capacity')
     .waitForExist('.plot-container.plotly')
-    .waitForExist('.heading-large')
-    .getValue('.heading-large', function (title) {
+    .waitForExist('.heading-secondary')
+    .getValue('.heading-secondary', function (title) {
       expect(title).to.equal('Region Capacity')
     })
   })
 
   it('should navigate to the team caseload capacity screen', () => {
-    return browser.url('/team/' + inserts.filter((item) => item.table === 'team')[0].id + '/caseload-capacity')
+    return browser.url('/team/' + workloadOwnerIds.filter((item) => item.table === 'team')[0].id + '/caseload-capacity')
       .waitForExist('.plot-container.plotly')
-      .waitForExist('.heading-large')
-      .getValue('.heading-large', function (title) {
+      .waitForExist('.heading-secondary')
+      .getValue('.heading-secondary', function (title) {
         expect(title).to.equal('Team Capacity')
       })
   })
