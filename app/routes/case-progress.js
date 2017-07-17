f08fb139a5370fb9acfba9e570a0a78c2e7cb4cb
Damien Coney <d.coney@kainos.com>
2017-07-17 11:28:20 +0100

    636: Updating non js version and graph table to accept arrays of data.

--- app/routes/case-progress.js
+++ app/routes/case-progress.js
@@ -1,16 +1,10 @@
 const getCaseProgress = require('../services/get-case-progress')
-const orgUnit = require('../constants/organisation-unit')
 const getSubNav = require('../services/get-sub-nav')
 
 module.exports = function (router) {
   router.get('/:organisationLevel/:id/case-progress', function (req, res, next) {
     var id = req.params.id
     var organisationLevel = req.params.organisationLevel
-
-    // Currently only dealing with Offender Managers
-    if (organisationLevel !== orgUnit.OFFENDER_MANAGER.name) {
-      throw new Error(organisationLevel + ' should be offender-manager')
-    }
 
     var caseProgressPromise = getCaseProgress(id, organisationLevel)
 
@@ -20,7 +14,7 @@ module.exports = function (router) {
         subTitle: result.subTitle,
         breadcrumbs: result.breadcrumbs,
         subNav: getSubNav(id, organisationLevel, req.path),
-        caseProgress: result.caseProgress
+        caseProgressList: result.caseProgressList
       })
     })
   })
