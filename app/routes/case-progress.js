afe29ba6c5ffc88cc89a9ce92d538cb7adf3551d
Caoimhe Boyle <c.boyle@kainos.com>
2017-07-18 14:25:04 +0100

    636: Add national id logic to case progress routes

--- app/routes/case-progress.js
+++ app/routes/case-progress.js
@@ -1,10 +1,15 @@
 const getCaseProgress = require('../services/get-case-progress')
 const getSubNav = require('../services/get-sub-nav')
+const organisationUnit = require('../constants/organisation-unit')
 
 module.exports = function (router) {
   router.get('/:organisationLevel/:id/case-progress', function (req, res, next) {
-    var id = req.params.id
     var organisationLevel = req.params.organisationLevel
+    var id
+
+    if (organisationLevel !== organisationUnit.NATIONAL.name) {
+      id = req.params.id
+    }
 
     var caseProgressPromise = getCaseProgress(id, organisationLevel)
 
