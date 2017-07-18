951638fe79b179096c11912d3a15283bc0b13dd8
Caoimhe Boyle <c.boyle@kainos.com>
2017-07-18 11:51:31 +0100

    636: Update case progress query to include name

--- app/services/data/get-caseload-progress.js
+++ app/services/data/get-caseload-progress.js
@@ -7,13 +7,14 @@ module.exports = function (id, type) {
   var table = orgUnit.caseProgressView
 
   return knex(table)
-    .where(table + '.id', id)
-    .select(table + '.community_last_16_weeks AS communityLast16Weeks',
-            table + '.license_last_16_weeks AS licenseLast16Weeks',
-            table + '.total_cases AS totalCases',
-            table + '.warrants_total AS warrantsTotal',
-            table + '.overdue_terminations_total AS overdueTerminationsTotal',
-            table + '.unpaid_work_total AS unpaidWorkTotal')
+    .where('id', id)
+    .select('name',
+            'community_last_16_weeks AS communityLast16Weeks',
+            'license_last_16_weeks AS licenseLast16Weeks',
+            'total_cases AS totalCases',
+            'warrants_total AS warrantsTotal',
+            'overdue_terminations_total AS overdueTerminationsTotal',
+            'unpaid_work_total AS unpaidWorkTotal')
     .then(function (results) {
       return results
     })
