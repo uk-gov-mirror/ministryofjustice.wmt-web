e4ec158005ba1272ebc1429cbacfcd6cf45f80a1
Caoimhe Boyle <c.boyle@kainos.com>
2017-07-19 11:16:59 +0100

    636: Add e2e tests for view case progress (not yet ran)

--- test/e2e/view-caseload-capacity.js
+++ test/e2e/view-caseload-capacity.js
@@ -15,6 +15,7 @@ describe('View your caseload capacity flow', () => {
   it('should navigate to the workload owner caseload capacity screen', () => {
     return browser.url('/offender-manager/' + workloadOwnerIds.filter((item) => item.table === 'workload_owner')[0].id + '/caseload-capacity')
       .waitForExist('.breadcrumbs')
+      .waitForExist('.sln-subnav')
       .waitForExist('.plot-container.plotly')
       .waitForExist('.heading-secondary')
       .getValue('.heading-secondary', function (title) {
@@ -22,9 +23,21 @@ describe('View your caseload capacity flow', () => {
       })
   })
 
+  it('should navigate to the team caseload capacity screen', () => {
+    return browser.url('/team/' + workloadOwnerIds.filter((item) => item.table === 'team')[0].id + '/caseload-capacity')
+      .waitForExist('.breadcrumbs')
+      .waitForExist('.sln-subnav')
+      .waitForExist('.plot-container.plotly')
+      .waitForExist('.heading-secondary')
+      .getValue('.heading-secondary', function (title) {
+        expect(title).to.equal('Team')
+      })
+  })
+
   it('should navigate to the ldu caseload capacity screen', () => {
     return browser.url('/ldu/' + workloadOwnerIds.filter((item) => item.table === 'ldu')[0].id + '/caseload-capacity')
       .waitForExist('.breadcrumbs')
+      .waitForExist('.sln-subnav')
       .waitForExist('.plot-container.plotly')
       .waitForExist('.heading-secondary')
       .getValue('.heading-secondary', function (title) {
@@ -35,6 +48,7 @@ describe('View your caseload capacity flow', () => {
   it('should navigate to the region caseload capacity screen', () => {
     return browser.url('/region/' + workloadOwnerIds.filter((item) => item.table === 'region')[0].id + '/caseload-capacity')
     .waitForExist('.breadcrumbs')
+    .waitForExist('.sln-subnav')
     .waitForExist('.plot-container.plotly')
     .waitForExist('.heading-secondary')
     .getValue('.heading-secondary', function (title) {
@@ -42,13 +56,5 @@ describe('View your caseload capacity flow', () => {
     })
   })
 
-  it('should navigate to the team caseload capacity screen', () => {
-    return browser.url('/team/' + workloadOwnerIds.filter((item) => item.table === 'team')[0].id + '/caseload-capacity')
-
-      .waitForExist('.plot-container.plotly')
-      .waitForExist('.heading-secondary')
-      .getValue('.heading-secondary', function (title) {
-        expect(title).to.equal('Team')
-      })
-  })
+  // TODO Add in National level
 })
--- test/e2e/view-caseload-progress.js
+++ test/e2e/view-caseload-progress.js
@@ -14,7 +14,7 @@ describe('View caseload progress flow', () => {
 
   it('should navigate to the workload owner caseload progress screen', () => {
     return browser.url('/offender-manager/' + workloadOwnerIds.filter((item) => item.table === 'workload_owner')[0].id + '/caseload-capacity')
-      .waitForExist('.breadcrumbs')
+      .waitForExist('.sln-subnav')
       .waitForExist('.sln-subnav')
       // Check the href for case progress using the id exists
       .click('[href="/offender-manager/' + workloadOwnerIds.filter((item) => item.table === 'workload_owner')[0].id + '/case-progress"]')
@@ -22,6 +22,58 @@ describe('View caseload progress flow', () => {
       .waitForExist('.js-plotly-plot')
       .getValue('.sln-page-subtitle', function (title) {
         expect(title).to.equal('Offender Manager')
+      })
+  })
+
+  it('should navigate to the team caseload progress screen', () => {
+    return browser.url('/team/' + workloadOwnerIds.filter((item) => item.table === 'team')[0].id + '/case-progress')
+      .waitForExist('.breadcrumbs')
+      .waitForExist('.sln-subnav')
+      .waitForExist('[href="/team/' + workloadOwnerIds.filter((item) => item.table === 'team')[0].id + '/caseload-capacity"]')
+      .waitForExist('[href="/team/' + workloadOwnerIds.filter((item) => item.table === 'team')[0].id + '/case-progress"]')
+      .waitForExist('.sln-page-subtitle')
+      .waitForExist('.js-plotly-plot')
+      .getValue('.sln-page-subtitle', function (title) {
+        expect(title).to.equal('Team')
+      })
+  })
+
+  it('should navigate to the ldu caseload progress screen', () => {
+    return browser.url('/ldu/' + workloadOwnerIds.filter((item) => item.table === 'ldu')[0].id + '/case-progress')
+      .waitForExist('.breadcrumbs')
+      .waitForExist('.sln-subnav')
+      .waitForExist('[href="/ldu/' + workloadOwnerIds.filter((item) => item.table === 'ldu')[0].id + '/caseload-capacity"]')
+      .waitForExist('[href="/ldu/' + workloadOwnerIds.filter((item) => item.table === 'ldu')[0].id + '/case-progress"]')
+      .waitForExist('.sln-page-subtitle')
+      .waitForExist('.js-plotly-plot')
+      .getValue('.sln-page-subtitle', function (title) {
+        expect(title).to.equal('LDU')
+      })
+  })
+
+  it('should navigate to the region caseload progress screen', () => {
+    return browser.url('/region/' + workloadOwnerIds.filter((item) => item.table === 'region')[0].id + '/case-progress')
+      .waitForExist('.breadcrumbs')
+      .waitForExist('.sln-subnav')
+      .waitForExist('[href="/region/' + workloadOwnerIds.filter((item) => item.table === 'region')[0].id + '/caseload-capacity"]')
+      .waitForExist('[href="/region/' + workloadOwnerIds.filter((item) => item.table === 'region')[0].id + '/case-progress"]')
+      .waitForExist('.sln-page-subtitle')
+      .waitForExist('.js-plotly-plot')
+      .getValue('.sln-page-subtitle', function (title) {
+        expect(title).to.equal('Region')
+      })
+  })
+
+  it('should navigate to the national caseload progress screen', () => {
+    return browser.url('/hmpps/' + workloadOwnerIds.filter((item) => item.table === 'region')[0].id + '/case-progress')
+      .waitForExist('.sln-subnav')
+      .waitForExist('[href="/hmpps/' + workloadOwnerIds.filter((item) => item.table === 'region')[0].id + '/caseload-capacity"]')
+      .waitForExist('[href="/hmpps/' + workloadOwnerIds.filter((item) => item.table === 'region')[0].id + '/case-progress"]')
+      .waitForExist('.sln-page-subtitle')
+      .waitForExist('.js-plotly-plot')
+      .isExisting('.breadcrumbs').should.eventually.equal(false)
+      .getValue('.sln-page-subtitle', function (title) {
+        expect(title).to.equal('HMPPS') // This should fail because of case
       })
   })
 })
