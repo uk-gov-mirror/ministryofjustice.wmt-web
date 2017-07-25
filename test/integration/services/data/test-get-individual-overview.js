45fd1963f2d6b92049f6695dce563343451fda72
Caoimhe Boyle <c.boyle@kainos.com>
2017-07-25 11:17:49 +0100

    678: Add query for individual overview details
    Update data helper used in integration tests to include data needed
    to test individual overview

---
+++ test/integration/services/data/test-get-individual-overview.js
@@ -0,0 +1,41 @@
+const expect = require('chai').expect
+
+const dataHelper = require('../../../helpers/data/aggregated-data-helper')
+const getIndividualOverview = require('../../../../app/services/data/get-individual-overview')
+
+var inserts = []
+
+overview = {
+  grade: 'PO',
+  teamName: 'Test Team',
+  availablePoints: 10,
+  totalPoints: 20,
+  cases: 5,
+  contractedHours: 40,
+  reduction: 3
+}
+
+describe('services/data/get-individual-overview', function () {
+  before(function (done) {
+    dataHelper.addWorkloadCapacitiesForOffenderManager()
+      .then(function (builtInserts) {
+        inserts = builtInserts
+        done()
+      })
+  })
+
+  it('should retrieve the overview details for a workload owner', function (done) {
+    getIndividualOverview(inserts.filter((item) => item.table === 'workload_owner')[0].id)
+    .then(function (results) {
+      var insertedTeamId = inserts.filter((item) => item.table === 'team')[0].id
+      expect(results).to.be.an('object')
+      expect(results).to.eql(Object.assign({}, overview, {teamId: insertedTeamId}))
+      done()
+    })
+  })
+
+  after(function (done) {
+    dataHelper.removeInsertedData(inserts)
+      .then(() => done())
+  })
+})
