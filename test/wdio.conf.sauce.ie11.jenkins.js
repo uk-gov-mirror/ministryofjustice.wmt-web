944a74f6ae906772204ed51a30d81f5ae4a31565
Andrew Wright <a.wright@kainos.com>
2017-08-01 17:38:21 +0100

    Rename base URL environment variable

--- test/wdio.conf.sauce.ie11.jenkins.js
+++ test/wdio.conf.sauce.ie11.jenkins.js
@@ -4,7 +4,7 @@ exports.config = wdioConfHelper({
   services: ['sauce'],
   user: process.env.SAUCE_USERNAME,
   key: process.env.SAUCE_ACCESS_KEY,
-  baseUrl: process.env.WMT_WEB_TEST_BASEURL || 'http://localhost:3000',
+  baseUrl: process.env.WMT_BASE_URL || 'http://localhost:3000',
   capabilities: [{
     maxInstances: 1,
     browserName: 'internet explorer',
