e8edbf0b65012663572435e3b89ab4d6277f8889
Dereje Debella <derejed@lion.kainos.com>
2017-08-21 16:13:24 +0100

    822: Extend caseload unit test to include LDU level calls.

--- test/unit/services/test-get-caseload.js
+++ test/unit/services/test-get-caseload.js
@@ -17,7 +17,7 @@ var CASE_TYPE = {
 const CASELOAD = {
   name: 'Todd Umptious',
   caseType: CASE_TYPE.CUSTODY,
-  grade: 'PO',
+  gradeCode: 'PO',
   teamId: '1',
   linkId: '2',
   totalCases: 3,
@@ -31,19 +31,19 @@ const CASELOAD = {
   a: 0
 }
 
-const OVERALL_CASELOAD = Object.assign({}, CASELOAD, {totalCases: 18, untiered: 3, d2: 3, c1: 3, b1: 3})
+const OVERALL_CASELOAD = Object.assign({}, CASELOAD, { totalCases: 18, untiered: 3, d2: 3, c1: 3, b1: 3 })
 
-const CUSTODY_CASELOAD = Object.assign({}, CASELOAD, {caseType: CASE_TYPE.CUSTODY})
-const COMMUNITY_CASELOAD = Object.assign({}, CASELOAD, {totalCases: 6}, {caseType: CASE_TYPE.COMMUNITY})
-const LICENSE_CASELOAD = Object.assign({}, CASELOAD, {totalCases: 9}, {caseType: CASE_TYPE.LICENSE})
+const CUSTODY_CASELOAD = Object.assign({}, CASELOAD, { caseType: CASE_TYPE.CUSTODY })
+const COMMUNITY_CASELOAD = Object.assign({}, CASELOAD, { totalCases: 6 }, { caseType: CASE_TYPE.COMMUNITY })
+const LICENSE_CASELOAD = Object.assign({}, CASELOAD, { totalCases: 9 }, { caseType: CASE_TYPE.LICENSE })
 
-const CUSTODY_CASELOAD_1 = Object.assign({}, CASELOAD, {linkId: 3}, {caseType: CASE_TYPE.CUSTODY})
-const COMMUNITY_CASELOAD_1 = Object.assign({}, CASELOAD, {linkId: 3}, {totalCases: 6}, {caseType: CASE_TYPE.COMMUNITY})
-const LICENSE_CASELOAD_1 = Object.assign({}, CASELOAD, {linkId: 3}, {totalCases: 9}, {caseType: CASE_TYPE.LICENSE})
+const CUSTODY_CASELOAD_1 = Object.assign({}, CASELOAD, { linkId: 3 }, { caseType: CASE_TYPE.CUSTODY })
+const COMMUNITY_CASELOAD_1 = Object.assign({}, CASELOAD, { linkId: 3 }, { totalCases: 6 }, { caseType: CASE_TYPE.COMMUNITY })
+const LICENSE_CASELOAD_1 = Object.assign({}, CASELOAD, { linkId: 3 }, { totalCases: 9 }, { caseType: CASE_TYPE.LICENSE })
 
-const CUSTODY_CASELOAD_2 = Object.assign({}, CASELOAD, {linkId: 4}, {caseType: CASE_TYPE.CUSTODY})
-const COMMUNITY_CASELOAD_2 = Object.assign({}, CASELOAD, {linkId: 4}, {totalCases: 6}, {caseType: CASE_TYPE.COMMUNITY})
-const LICENSE_CASELOAD_2 = Object.assign({}, CASELOAD, {linkId: 4}, {totalCases: 9}, {caseType: CASE_TYPE.LICENSE})
+const CUSTODY_CASELOAD_2 = Object.assign({}, CASELOAD, { linkId: 4 }, { caseType: CASE_TYPE.CUSTODY })
+const COMMUNITY_CASELOAD_2 = Object.assign({}, CASELOAD, { linkId: 4 }, { totalCases: 6 }, { caseType: CASE_TYPE.COMMUNITY })
+const LICENSE_CASELOAD_2 = Object.assign({}, CASELOAD, { linkId: 4 }, { totalCases: 9 }, { caseType: CASE_TYPE.LICENSE })
 
 const TEAM_CASELOAD = [CUSTODY_CASELOAD, CUSTODY_CASELOAD_1, CUSTODY_CASELOAD_2,
   COMMUNITY_CASELOAD, COMMUNITY_CASELOAD_1, COMMUNITY_CASELOAD_2,
@@ -54,22 +54,24 @@ var breadcrumbs = breadcrumbHelper.TEAM_BREADCRUMBS
 var expectedTitle = breadcrumbs[0].title
 
 var getCaseload
-var getTeamCaseload
+var getCaseloadDetail
 var getBreadcrumbs
 
 beforeEach(function () {
-  getTeamCaseload = sinon.stub()
+  getCaseloadDetail = sinon.stub()
   getBreadcrumbs = sinon.stub().returns(breadcrumbs)
   getCaseload =
-      proxyquire('../../../app/services/get-caseload',
-        {'./data/get-caseload': getTeamCaseload,
-          './get-breadcrumbs': getBreadcrumbs})
+    proxyquire('../../../app/services/get-caseload',
+      {
+        './data/get-caseload': getCaseloadDetail,
+        './get-breadcrumbs': getBreadcrumbs
+      })
 })
 
 describe('services/get-caseload', function () {
   it('should return a results object with breadcrumbs, title and subtitle for a team', function () {
     var teamName = orgUnitConstant.TEAM.name
-    getTeamCaseload.withArgs(id).resolves(OVERALL_CASELOAD)
+    getCaseloadDetail.withArgs(id).resolves(OVERALL_CASELOAD)
     return getCaseload(id, teamName).then(function (result) {
       var teamSubtitle = orgUnitFinder('name', teamName).displayText
       expect(getBreadcrumbs).to.have.been.called //eslint-disable-line
@@ -79,69 +81,96 @@ describe('services/get-caseload', function () {
     })
   })
 
-  it('should call get-team-caseload and return a results object with the overall caseload details for a team', function () {
+  it('should call get-caseload and return a results object with the overall caseload details for a team', function () {
     var teamName = orgUnitConstant.TEAM.name
-    getTeamCaseload.withArgs(id).resolves(TEAM_CASELOAD)
+    getCaseloadDetail.withArgs(id).resolves(TEAM_CASELOAD)
     return getCaseload(id, teamName).then(function (result) {
-      assert(getTeamCaseload.called)
+      assert(getCaseloadDetail.called)
       expect(result.overallCaseloadDetails.length).to.eql(3)
       expect(result.overallCaseloadDetails[0]).to.eql(OVERALL_CASELOAD)
-      expect(result.overallCaseloadDetails[1]).to.eql(Object.assign({}, OVERALL_CASELOAD, {linkId: 3}))
-      expect(result.overallCaseloadDetails[2]).to.eql(Object.assign({}, OVERALL_CASELOAD, {linkId: 4}))
+      expect(result.overallCaseloadDetails[1]).to.eql(Object.assign({}, OVERALL_CASELOAD, { linkId: 3 }))
+      expect(result.overallCaseloadDetails[2]).to.eql(Object.assign({}, OVERALL_CASELOAD, { linkId: 4 }))
     })
   })
 
-  it('should call get-team-caseload and return a caseload object with the CUSTODY casetype for a team', function () {
+  it('should call get-caseload and return a caseload object with the CUSTODY casetype for a team', function () {
     var teamName = orgUnitConstant.TEAM.name
-    getTeamCaseload.withArgs(id).resolves(TEAM_CASELOAD)
+    getCaseloadDetail.withArgs(id).resolves(TEAM_CASELOAD)
     return getCaseload(id, teamName).then(function (result) {
-      assert(getTeamCaseload.called)
+      assert(getCaseloadDetail.called)
       expect(result.custodyCaseloadDetails[0]).to.eql(CUSTODY_CASELOAD)
     })
   })
 
-  it('should call get-team-caseload and return a caseload object with the COMMUNITY casetype for a team', function () {
+  it('should call get-caseload and return a caseload object with the COMMUNITY casetype for a team', function () {
     var teamName = orgUnitConstant.TEAM.name
-    getTeamCaseload.withArgs(id).resolves(TEAM_CASELOAD)
+    getCaseloadDetail.withArgs(id).resolves(TEAM_CASELOAD)
     return getCaseload(id, teamName).then(function (result) {
-      assert(getTeamCaseload.called)
+      assert(getCaseloadDetail.called)
       expect(result.communityCaseloadDetails[0]).to.eql(COMMUNITY_CASELOAD)
     })
   })
 
-  it('should call get-team-caseload and return a caseload object with the LICENSE casetype for a team', function () {
+  it('should call get-caseload and return a caseload object with the LICENSE casetype for a team', function () {
     var teamName = orgUnitConstant.TEAM.name
-    getTeamCaseload.withArgs(id).resolves(TEAM_CASELOAD)
+    getCaseloadDetail.withArgs(id).resolves(TEAM_CASELOAD)
     return getCaseload(id, teamName).then(function (result) {
-      assert(getTeamCaseload.called)
+      assert(getCaseloadDetail.called)
       expect(result.licenseCaseloadDetails[0]).to.eql(LICENSE_CASELOAD)
     })
   })
 
-  it('should call get-team-caseload and return caseload total summary for CUSTODY casetype for a team', function () {
+  it('should call get-caseload and return caseload total summary for CUSTODY casetype for a team', function () {
     var teamName = orgUnitConstant.TEAM.name
-    getTeamCaseload.withArgs(id).resolves(TEAM_CASELOAD)
+    getCaseloadDetail.withArgs(id).resolves(TEAM_CASELOAD)
     return getCaseload(id, teamName).then(function (result) {
-      assert(getTeamCaseload.called)
+      assert(getCaseloadDetail.called)
       expect(result.custodyTotalSummary).to.eql(9)
     })
   })
 
-  it('should call get-team-caseload and return caseload total summary for COMMUNITY casetype for a team', function () {
+  it('should call get-caseload and return caseload total summary for COMMUNITY casetype for a team', function () {
     var teamName = orgUnitConstant.TEAM.name
-    getTeamCaseload.withArgs(id).resolves(TEAM_CASELOAD)
+    getCaseloadDetail.withArgs(id).resolves(TEAM_CASELOAD)
     return getCaseload(id, teamName).then(function (result) {
-      assert(getTeamCaseload.called)
+      assert(getCaseloadDetail.called)
       expect(result.communityTotalSummary).to.eql(18)
     })
   })
 
-  it('should call get-team-caseload and return caseload total summary for LICENSE casetype for a team', function () {
+  it('should call get-caseload and return caseload total summary for LICENSE casetype for a team', function () {
     var teamName = orgUnitConstant.TEAM.name
-    getTeamCaseload.withArgs(id).resolves(TEAM_CASELOAD)
+    getCaseloadDetail.withArgs(id).resolves(TEAM_CASELOAD)
     return getCaseload(id, teamName).then(function (result) {
-      assert(getTeamCaseload.called)
+      assert(getCaseloadDetail.called)
       expect(result.licenseTotalSummary).to.eql(27)
+    })
+  })
+
+  it('should return a results object with breadcrumbs, title and subtitle for LDU', function () {
+    var lduName = orgUnitConstant.LDU.name
+    getCaseloadDetail.withArgs(id).resolves(OVERALL_CASELOAD)
+    return getCaseload(id, lduName).then(function (result) {
+      var lduSubtitle = orgUnitFinder('name', lduName).displayText
+      expect(getBreadcrumbs).to.have.been.called //eslint-disable-line
+      expect(result.breadcrumbs).to.eql(breadcrumbs)
+      expect(result.subTitle).to.eql(lduSubtitle)
+      expect(result.title).to.eql(expectedTitle)
+    })
+  })
+
+  it('should call get-caseload and return ldu caseload object', function () {
+    var lduName = orgUnitConstant.LDU.name
+    getCaseloadDetail.withArgs(id).resolves(TEAM_CASELOAD)
+    return getCaseload(id, lduName).then(function (result) {
+      assert(getCaseloadDetail.called)
+      expect(result.lduCaseloadDetails.length).to.eql(3)
+      expect(result.lduCaseloadDetails[0].linkId).to.eql(CASELOAD.linkId)
+      expect(result.lduCaseloadDetails[0].name).to.eql(CASELOAD.name)
+      expect(result.lduCaseloadDetails[0].grades.length).to.eql(3)
+      expect(result.lduCaseloadDetails[0].grades[0].untiered).to.be.within(33.33, 33.334)
+      expect(result.lduCaseloadDetails[0].grades[1].untiered).to.be.within(33.33, 33.334)
+      expect(result.lduCaseloadDetails[0].grades[2].untiered).to.be.within(33.33, 33.334)
     })
   })
 })
