c68077fce7569afd8554ac53fb3b7a7fb08b4b57
Damien Coney <d.coney@kainos.com>
2017-08-21 12:27:52 +0100

    Updating css classes for selenium tests and fixing failing test

--- test/e2e/view-national-page.js
+++ test/e2e/view-national-page.js
@@ -3,17 +3,17 @@ const expect = require('chai').expect
 describe('View landing Page and national view', () => {
   it('should navigate to the landing page and have a link present', () => {
     return browser.url('/')
-      .waitForExist('#head-xlarge')
-      .waitForExist('.caseload-capacity-link')
-      .getText('.caseload-capacity-link')
+      .waitForExist('.sln-overview-heading')
+      .waitForExist('.sln-capacity-link')
+      .getText('.sln-capacity-link')
       .then(function (text) {
         expect(text).to.equal('National Level Capacity')
       })
   })
   it('should click the link and the national view should appear', () => {
     return browser.url('/')
-      .waitForExist('.caseload-capacity-link')
-      .click('.caseload-capacity-link')
+      .waitForExist('.sln-capacity-link')
+      .click('.sln-capacity-link')
       .waitForExist('.breadcrumbs')
       .waitForExist('.sln-subnav')
       .waitForExist('.sln-page-subtitle')
