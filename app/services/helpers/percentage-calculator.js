4a21805e2d5312fe43649873c01aa9365be31b85
Caoimhe Boyle <c.boyle@kainos.com>
2017-08-21 13:27:09 +0100

    822: Renamed vars

--- app/services/helpers/percentage-calculator.js
+++ app/services/helpers/percentage-calculator.js
@@ -1,37 +1,33 @@
 const caseloadHelper = require('./caseload-helper.js')
 
-// Rename basically everything
-module.exports = function (object) {
+module.exports = function (caseloadTotalsByGrade) {
   var percentageResults = []
+  var caseloadTotalsByTeam = caseloadHelper.getOverallCaseload(caseloadTotalsByGrade)
 
-  var teamTotals = caseloadHelper.getOverallCaseload(object)
+  // For each team, create one entry in the new results set with one 'grade' sub-object per grade
+  for (var team in caseloadTotalsByTeam) {
+    var newTeamEntry = { linkId: caseloadTotalsByTeam[team].linkId, name: caseloadTotalsByTeam[team].name }
+    var teamGradeRecords = caseloadTotalsByGrade.filter((row) => row.linkId === caseloadTotalsByTeam[team].linkId)
 
-  for (var team in teamTotals) {
-    var entry = {linkId: teamTotals[team].linkId, name: teamTotals[team].name}
-
-    var gradeRecords = object.filter((row) => row.linkId === teamTotals[team].linkId)
-    var newGradeRecords = []
-    for (var record in gradeRecords) {
+    var percentageGradeRecords = []
+    for (var record in teamGradeRecords) {
       var newGradeRecord = {
-        gradeCode: gradeRecords[record].gradeCode,
-        untiered: calculatePercentage(gradeRecords[record].untiered, teamTotals[team].untiered),
-        d2: calculatePercentage(gradeRecords[record].d2, teamTotals[team].d2),
-        d1: calculatePercentage(gradeRecords[record].d1, teamTotals[team].d1),
-        c2: calculatePercentage(gradeRecords[record].c2, teamTotals[team].c2),
-        c1: calculatePercentage(gradeRecords[record].c1, teamTotals[team].c1),
-        b2: calculatePercentage(gradeRecords[record].b2, teamTotals[team].b2),
-        b1: calculatePercentage(gradeRecords[record].b1, teamTotals[team].b1),
-        a: calculatePercentage(gradeRecords[record].a, teamTotals[team].a),
-        totalCases: calculatePercentage(gradeRecords[record].totalCases, teamTotals[team].totalCases)
+        gradeCode: teamGradeRecords[record].gradeCode,
+        untiered: calculatePercentage(teamGradeRecords[record].untiered, caseloadTotalsByTeam[team].untiered),
+        d2: calculatePercentage(teamGradeRecords[record].d2, caseloadTotalsByTeam[team].d2),
+        d1: calculatePercentage(teamGradeRecords[record].d1, caseloadTotalsByTeam[team].d1),
+        c2: calculatePercentage(teamGradeRecords[record].c2, caseloadTotalsByTeam[team].c2),
+        c1: calculatePercentage(teamGradeRecords[record].c1, caseloadTotalsByTeam[team].c1),
+        b2: calculatePercentage(teamGradeRecords[record].b2, caseloadTotalsByTeam[team].b2),
+        b1: calculatePercentage(teamGradeRecords[record].b1, caseloadTotalsByTeam[team].b1),
+        a: calculatePercentage(teamGradeRecords[record].a, caseloadTotalsByTeam[team].a),
+        totalCases: calculatePercentage(teamGradeRecords[record].totalCases, caseloadTotalsByTeam[team].totalCases)
       }
-      newGradeRecords.push(newGradeRecord)
+      percentageGradeRecords.push(newGradeRecord)
     }
-
-    entry = Object.assign({}, entry, {grades: newGradeRecords})
-
-    percentageResults.push(entry)
+    newTeamEntry = Object.assign({}, newTeamEntry, {grades: percentageGradeRecords})
+    percentageResults.push(newTeamEntry)
   }
-
   return percentageResults
 }
 
