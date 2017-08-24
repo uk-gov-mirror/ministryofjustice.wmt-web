4758a5e2b9733eb17a5db7c6aee8873828cd6d7b
Caoimhe Boyle <c.boyle@kainos.com>
2017-08-24 15:28:10 +0100

    871: Update overview table link to default for each org unit
    Remove '/overview' from the links in the overview table so clicking this link will
    go to the default page for an org unit (which is currently overview)
    
    Also, add to overview e2e tests to test navigation between levels

--- test/e2e/view-caseload-overview.js
+++ test/e2e/view-caseload-overview.js
@@ -5,6 +5,11 @@ const dataHelper = require('../helpers/data/aggregated-data-helper')
 var workloadOwnerIds = []
 var workloadOwnerId
 var workloadOwnerGrade
+var workloadOwnerDefaultUrl
+var teamDefaultUrl
+var lduDefaultUrl
+var regionDefaultUrl
+var nationalDefaultUrl
 
 describe('View overview', function () {
   before(function () {
@@ -12,6 +17,11 @@ describe('View overview', function () {
       .then(function (results) {
         workloadOwnerIds = results
         workloadOwnerId = workloadOwnerIds.filter((item) => item.table === 'workload_owner')[0].id
+        workloadOwnerDefaultUrl = '/offender-manager/' + workloadOwnerId
+        teamDefaultUrl = '/team/' + workloadOwnerIds.filter((item) => item.table === 'team')[0].id
+        lduDefaultUrl = '/ldu/' + workloadOwnerIds.filter((item) => item.table === 'ldu')[0].id
+        regionDefaultUrl = '/region/' + workloadOwnerIds.filter((item) => item.table === 'region')[0].id
+        nationalDefaultUrl = '/hmpps/0'
         return results
       })
       .then(function (builtInserts) {
@@ -23,7 +33,7 @@ describe('View overview', function () {
   })
 
   it('should navigate to the workload owner overview page', function () {
-    return browser.url('/offender-manager/' + workloadOwnerIds.filter((item) => item.table === 'workload_owner')[0].id + '/overview')
+    return browser.url(workloadOwnerDefaultUrl + '/overview')
       .waitForExist('.breadcrumbs')
       .waitForExist('.sln-subnav')
       .waitForExist('.sln-grade')
@@ -34,10 +44,10 @@ describe('View overview', function () {
   })
 
   it('should navigate to the team overview page', function () {
-    return browser.url('/team/' + workloadOwnerIds.filter((item) => item.table === 'team')[0].id + '/overview')
+    return browser.url(teamDefaultUrl + '/overview')
       .waitForExist('.breadcrumbs')
       .waitForExist('.sln-subnav')
-      .waitForExist('[href="/offender-manager/' + workloadOwnerIds.filter((item) => item.table === 'workload_owner')[0].id + '/overview"]')
+      .waitForExist('[href="' + workloadOwnerDefaultUrl + '"]')
       .getText('.sln-table-org-level')
       .then(function (text) {
         expect(text).to.equal('Team')
@@ -45,10 +55,10 @@ describe('View overview', function () {
   })
 
   it('should naviagte to the ldu overview page', function () {
-    return browser.url('/ldu/' + workloadOwnerIds.filter((item) => item.table === 'ldu')[0].id + '/overview')
+    return browser.url(lduDefaultUrl + '/overview')
       .waitForExist('.breadcrumbs')
       .waitForExist('.sln-subnav')
-      .waitForExist('[href="/team/' + workloadOwnerIds.filter((item) => item.table === 'team')[0].id + '/overview"]')
+      .waitForExist('[href="' + teamDefaultUrl + '"]')
       .getText('.sln-table-org-level')
       .then(function (text) {
         expect(text).to.equal('LDU Cluster')
@@ -56,10 +66,10 @@ describe('View overview', function () {
   })
 
   it('should naviagte to the region overview page', function () {
-    return browser.url('/region/' + workloadOwnerIds.filter((item) => item.table === 'region')[0].id + '/overview')
+    return browser.url(regionDefaultUrl + '/overview')
       .waitForExist('.breadcrumbs')
       .waitForExist('.sln-subnav')
-      .waitForExist('[href="/ldu/' + workloadOwnerIds.filter((item) => item.table === 'ldu')[0].id + '/overview"]')
+      .waitForExist('[href="' + lduDefaultUrl + '"]')
       .getText('.sln-table-org-level')
       .then(function (text) {
         expect(text).to.equal('Division')
@@ -67,11 +77,71 @@ describe('View overview', function () {
   })
 
   it('should navigate to the national overview page', function () {
-    return browser.url('/hmpps/0/overview')
+    return browser.url(nationalDefaultUrl + '/overview')
       .waitForExist('.breadcrumbs')
       .waitForExist('.sln-subnav')
-      .waitForExist('[href="/region/' + workloadOwnerIds.filter((item) => item.table === 'region')[0].id + '/overview"]')
+      .waitForExist('[href="' + regionDefaultUrl + '"]')
       .getText('.sln-table-org-level')
+      .then(function (text) {
+        expect(text).to.equal('National')
+      })
+  })
+
+  it('should allow the user to navigate down the org hierarchy from the national page', function () {
+    return browser.url(nationalDefaultUrl + '/overview')
+      .getText('.sln-page-subtitle')
+      .then(function (text) {
+        expect(text).to.equal('National')
+      })
+      .click('[href="' + regionDefaultUrl + '"]')
+      .getText('.sln-page-subtitle')
+      .then(function (text) {
+        expect(text).to.equal('Division')
+      })
+      .click('[href="' + lduDefaultUrl + '"]')
+      .getText('.sln-page-subtitle')
+      .then(function (text) {
+        expect(text).to.equal('LDU Cluster')
+      })
+      .click('[href="' + teamDefaultUrl + '"]')
+      .getText('.sln-page-subtitle')
+      .then(function (text) {
+        expect(text).to.equal('Team')
+      })
+      .click('[href="' + workloadOwnerDefaultUrl + '"]')
+      .getText('.sln-page-subtitle')
+      .then(function (text) {
+        expect(text).to.equal('Offender Manager')
+      })
+  })
+
+  it('should contain breadcrumbs which allow the user to navigate up the org hierarchy', function () {
+    return browser.url(workloadOwnerDefaultUrl)
+      .getText('.sln-page-subtitle')
+      .then(function (text) {
+        expect(text).to.equal('Offender Manager')
+      })
+      .waitForExist('[href="' + teamDefaultUrl + '"]')
+      .waitForExist('[href="' + lduDefaultUrl + '"]')
+      .waitForExist('[href="' + regionDefaultUrl + '"]')
+      .waitForExist('[href="' + nationalDefaultUrl + '"]')
+      .click('[href="' + teamDefaultUrl + '"]')
+      .getText('.sln-page-subtitle')
+      .then(function (text) {
+        expect(text).to.equal('Team')
+      })
+      .click('[href="' + lduDefaultUrl + '"]')
+      .getText('.sln-page-subtitle')
+      .then(function (text) {
+        expect(text).to.equal('LDU Cluster')
+      })
+      .click('[href="' + regionDefaultUrl + '"]')
+      .getText('.sln-page-subtitle')
+      .then(function (text) {
+        expect(text).to.equal('Division')
+      })
+      .click('[href="' + nationalDefaultUrl + '"]')
+      .getText('.sln-page-subtitle')
       .then(function (text) {
         expect(text).to.equal('National')
       })
