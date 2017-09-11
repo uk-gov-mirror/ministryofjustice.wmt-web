48a0bc88305d10e4cb9d72092bc791cf14bbaf4d
Damien Coney <d.coney@kainos.com>
2017-09-11 10:19:29 +0100

    Pushing for Caoimhe

--- app/services/workload-points-service.js
+++ app/services/workload-points-service.js
@@ -2,6 +2,8 @@ const getWorkloadPoints = require('./data/get-workload-points')
 // const getBreadcrumbs = require('./get-breadcrumbs')
 const updateWorkloadPoints = require('./data/update-workload-points-effective-to')
 const insertNewWorkloadPoints = require('./data/insert-workload-points')
+const getWorkloadBatchIdsAndWorkloadReport = require('./data/get-workload-batch-ids-and-workload-report-id')
+const createCalculateWorkloadPointsTaskForWorkloads = require('./data/get-workload-ids-for-workload')
 const Link = require('./domain/link')
 
 module.exports.getWorkloadPoints = function (id, organisationLevel) {
@@ -23,11 +25,13 @@ module.exports.getWorkloadPoints = function (id, organisationLevel) {
 }
 
 module.exports.updateWorkloadPoints = function (workloadPoints) {
-  var result = {}
-
-  return updateWorkloadPoints(workloadPoints.previousWpId).then(function (results) {
-    return insertNewWorkloadPoints(workloadPoints).then(function (results) {
-      return result
+  return updateWorkloadPoints(workloadPoints.previousWpId).then(function (updateResults) {
+    return insertNewWorkloadPoints(workloadPoints).then(function (insertResults) {
+      return getWorkloadBatchIdsAndWorkloadReport(workloadPoints.previousWpId).then(function (result) {
+        return createCalculateWorkloadPointsTaskForWorkloads(insertResults.workloadId, result.workloadReportId, result.numOfWorkloads).then(function (taskResults) {
+          return updateResults
+        })
+      })
     })
   })
 }
