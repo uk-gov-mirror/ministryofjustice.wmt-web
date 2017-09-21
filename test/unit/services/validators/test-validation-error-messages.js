10ff75b397ee4e2b7aa6ec556ba594345d2a13ff
Tom Swann <ts.swann@gmail.com>
2017-09-21 12:08:34 +0100

    Add missing tests for complete set of error messages

--- test/unit/services/validators/test-validation-error-messages.js
+++ test/unit/services/validators/test-validation-error-messages.js
@@ -1,4 +1,4 @@
-const ValidationErrorMessages = require('../../../../app/services/validators/validation-error-messages.js')
+const ValidationErrorMessages = require('../../../../app/services/validators/validation-error-messages')
 const expect = require('chai').expect
 
 const fieldName = 'Name'
@@ -27,6 +27,47 @@ describe('services/validators/validation-error-messages', function () {
     var max = 99
     const expected = `${fieldName} must be between ${min} and ${max}`
     var message = ValidationErrorMessages.getIsRangeMessage(fieldName, { min: min, max: max })
+    expect(message).to.equal(expected)
+  })
+
+  it('should return expected IsInteger validation error message', function () {
+    var min = 1
+    var max = 99
+    const expected = `${fieldName} must be a whole number between ${min} and ${max}`
+    var message = ValidationErrorMessages.getIsIntegerMessage(fieldName, { min: min, max: max })
+    expect(message).to.equal(expected)
+  })
+
+  it('should return expected IsFloat validation error message', function () {
+    var min = 0.9
+    var max = 37.5
+    const expected = `${fieldName} must be a number between ${min} and ${max}`
+    var message = ValidationErrorMessages.getIsFloatMessage(fieldName, { min: min, max: max })
+    expect(message).to.equal(expected)
+  })
+
+  it('should return expected InvalidDateFormatMessage validation error message', function () {
+    const expected = `${fieldName} was invalid`
+    var message = ValidationErrorMessages.getInvalidDateFormatMessage(fieldName)
+    expect(message).to.equal(expected)
+  })
+
+  it('should return expected FutureDateMessage validation error message', function () {
+    const expected = `${fieldName} must be in the future`
+    var message = ValidationErrorMessages.getFutureDateMessage(fieldName)
+    expect(message).to.equal(expected)
+  })
+
+  it('should return expected IsDateLaterThanMessage validation error message', function () {
+    const expected = `${fieldName} must be after the start date`
+    var message = ValidationErrorMessages.getIsDateLaterThanMessage(fieldName)
+    expect(message).to.equal(expected)
+  })
+
+  it('should return expected IsDateLaterThanMessage validation error message', function () {
+    const length = 100
+    const expected = `${fieldName} must be shorter than ${length} characters`
+    var message = ValidationErrorMessages.getIsLessThanLengthMessage(fieldName, { length: length })
     expect(message).to.equal(expected)
   })
 })
