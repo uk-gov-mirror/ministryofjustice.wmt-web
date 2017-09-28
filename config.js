646ec4089996bea90e8765a9cbe81aad2c8c050e
ddebella <25299873+ddebella@users.noreply.github.com>
2017-09-28 11:13:48 +0100

    751: change the default return address and don't save sessions if users are not logged in.

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   AUTHENTICATION_ENABLED: process.env.WMT_WEB_AUTHENTICATION_ENABLED || false,
   ACTIVE_DIRECTORY_ENTRY_POINT: process.env.WMT_ACTIVE_DIRECTORY_ENTRY_POINT || 'enter here', // SAML-P Sign-On Endpoint
   ACTIVE_DIRECTORY_ISSUER: process.env.WMT_ACTIVE_DIRECTORY_ISSUER || 'app id', // APP-ID
-  ACTIVE_DIRECTORY_RETURN_ADDRESS: process.env.WMT_ACTIVE_DIRECTORY_RETURN_ADDRESS || '/authentication-saml',
+  ACTIVE_DIRECTORY_RETURN_ADDRESS: process.env.WMT_ACTIVE_DIRECTORY_RETURN_ADDRESS || '/login',
   ACTIVE_DIRECTORY_DOMAIN: process.env.WMT_ACTIVE_DIRECTORY_DOMAIN || 'justice.gov.uk', // SAML CERT
 
   // Session and Cookie security (defaults for development)
@@ -15,7 +15,7 @@ module.exports = {
   SECURE_COOKIE: process.env.WMT_WEB_SECURE_COOKIE || 'false',
   SESSION_COOKIE_MAXAGE: process.env.WMT_WEB_SESSION_COOKIE_MAXAGE || '3600000', // 60 min default
   RESAVE_SESSION: process.env.WMT_WEB_SESSION_COOKIE_RESAVE || 'true',
-  SAVE_UNINITIALIZED_SESSION: process.env.WMT_WEB_SESSION_COOKIE_SAVE_UNINITIALISED || 'true',
+  SAVE_UNINITIALIZED_SESSION: process.env.WMT_WEB_SESSION_COOKIE_SAVE_UNINITIALISED || 'false',
 
   // DB
   DATABASE_SERVER: process.env.WMT_DB_SERVER || 'localhost',
