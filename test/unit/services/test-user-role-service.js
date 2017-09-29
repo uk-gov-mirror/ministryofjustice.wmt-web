b8a5d2bfed10420483117ae38200908deba4026b
ddebella <25299873+ddebella@users.noreply.github.com>
2017-09-29 16:18:29 +0100

    751: Fix failure test

--- test/unit/services/test-user-role-service.js
+++ test/unit/services/test-user-role-service.js
@@ -5,104 +5,114 @@ require('sinon-bluebird')
 const proxyquire = require('proxyquire')
 const config = require('../../../config')
 const userRole = require('../../../app/constants/user-roles.js')
+const UserRole = require('../../../app/services/domain/user-role')
 
 var STAFF_USER = 'Staff.Test@' + config.ACTIVE_DIRECTORY_DOMAIN
-var SYSTEM_USER = 'System.AdminTest@' + config.ACTIVE_DIRECTORY_DOMAIN
-var DATA_USER = 'Data.AdminTest@' + config.ACTIVE_DIRECTORY_DOMAIN
-var MANAGER_USER = 'Manager.Test@' + config.ACTIVE_DIRECTORY_DOMAIN
 
-var getRoleByUsername
+var STAFF_ROLE = { roleId: 0, role: userRole.STAFF }
+
 var userRoleService
+var getRoleByUsername
+var updateUserRole
+var addUserRole
+var getRole
+var getUser
+var addUser
+var removeUserRoleByUserId
+var removeUserByUsername
 
 beforeEach(function () {
-  getRoleByUsername = sinon.stub()
+  getRoleByUsername = sinon.stub().returns({ roleId: 0, role: 'Staff' })
+  updateUserRole = sinon.stub()
+  addUserRole = sinon.stub()
+  getRole = sinon.stub()
+  getUser = sinon.stub()
+  addUser = sinon.stub()
+  removeUserRoleByUserId = sinon.stub()
+  removeUserByUsername = sinon.stub()
 
   userRoleService =
     proxyquire('../../../app/services/user-role-service',
       {
-        './services/user-role-service': getRoleByUsername
+        './data/remove-user-role-by-user-id': removeUserRoleByUserId,
+        './data/remove-user-by-username': removeUserByUsername,
+        './data/get-user-role-by-username': getRoleByUsername,
+        './data/get-user-by-username': getUser,
+        './data/update-user-role': updateUserRole,
+        './data/insert-user-role': addUserRole,
+        './data/insert-user': addUser,
+        './data/get-role': getRole
       })
 })
 
 describe('services/user-role-service', function () {
-  describe('Get role by username', function () {
-    it('should return Staff role object with the right information', function () {
-      return userRoleService.getRoleByUsername(STAFF_USER)
-        .then(function (result) {
-          expect(result.roleId).to.equal(0)
-          expect(result.role).to.equal(userRole.STAFF)
-        })
-    })
-
-    it('should return System Admin user role object for a system admin user', function () {
-      return userRoleService.getRoleByUsername(SYSTEM_USER)
-        .then(function (result) {
-          expect(result.roleId).to.be.a('number')
-          expect(result.role).to.equal(userRole.SYSTEM_ADMIN)
-        })
-    })
-
-    it('should return Data Admin user role object for a data admin user', function () {
-      return userRoleService.getRoleByUsername(DATA_USER)
-        .then(function (result) {
-          expect(result.roleId).to.be.a('number')
-          expect(result.role).to.equal(userRole.DATA_ADMIN)
-        })
-    })
-
-    it('should return Manager Admin user role object for a manager user', function () {
-      return userRoleService.getRoleByUsername(MANAGER_USER)
-        .then(function (result) {
-          expect(result.roleId).to.be.a('number')
-          expect(result.role).to.equal(userRole.MANAGER)
-        })
-    })
+  it('should return a user role object with the right information', function () {
+    getRoleByUsername.resolves(STAFF_ROLE)
+    return userRoleService.getRoleByUsername(STAFF_USER)
+      .then(function (result) {
+        expect(result.roleId).to.equal(0)
+        expect(result.role).to.equal(userRole.STAFF)
+      })
   })
 
-  describe('Get role', function () {
-    it('should return a system admin role object with the right information', function () {
-      return userRoleService.getRole(userRole.SYSTEM_ADMIN)
-        .then(function (result) {
-          expect(result.id).to.be.a('number')
-          expect(result.role).to.equal(userRole.SYSTEM_ADMIN)
-        })
-    })
-    it('should return a data admin role object with the right information', function () {
-      return userRoleService.getRole(userRole.DATA_ADMIN)
-        .then(function (result) {
-          expect(result.id).to.be.a('number')
-          expect(result.role).to.equal(userRole.DATA_ADMIN)
-        })
-    })
-    it('should return a manager role object with the right information', function () {
-      return userRoleService.getRole(userRole.MANAGER)
-        .then(function (result) {
-          expect(result.id).to.be.a('number')
-          expect(result.role).to.equal(userRole.MANAGER)
-        })
-    })
+  it('should return a role object with the right information', function () {
+    getRole.resolves(STAFF_ROLE)
+    return userRoleService.getRole(userRole.STAFF)
+      .then(function (result) {
+        expect(result.roleId).to.be.a('number')
+        expect(result.role).to.equal(userRole.STAFF)
+      })
   })
 
-  describe('Get user', function () {
-    it('should return a user object with the right information', function () {
-      return userRoleService.getUser(MANAGER_USER)
-        .then(function (result) {
-          expect(result.id).to.be.a('number')
-          expect(result.username + '@' + config.ACTIVE_DIRECTORY_DOMAIN).to.equal(MANAGER_USER)
-        })
-    })
+  it('should return a user object with the right information', function () {
+    getUser.resolves({id: 1, username: STAFF_USER})
+    return userRoleService.getUser(STAFF_USER)
+      .then(function (result) {
+        expect(result.id).to.be.a('number')
+        expect(result.username).to.equal(STAFF_USER)
+      })
   })
 
-  describe('Add user', function () {
-    it('should add a user', function () {
-      return userRoleService.addUser(STAFF_USER)
-        .then(function (id) {
-          expect(id).to.be.a('number')
-          return userRoleService.removeUserByUsername(STAFF_USER)
-          .then(function (result) {
-            expect(result).to.be.greaterThan(0)
-          })
-        })
-    })
+  it('should add a user', function () {
+    addUser.resolves(1)
+    return userRoleService.addUser(STAFF_USER)
+      .then(function (result) {
+        expect(result).to.equal(1)
+      })
+  })
+
+  updateUserRole = sinon.stub()
+  addUserRole = sinon.stub()
+
+  it('should add a user role', function () {
+    addUserRole.resolves(1)
+    return userRoleService.addUserRole(new UserRole(1, 1, new Date(), 1))
+      .then(function (result) {
+        expect(result).to.equal(1)
+      })
+  })
+
+  it('should update a user role', function () {
+    updateUserRole.resolves(1)
+    return userRoleService.updateUserRole(new UserRole(1, 2, new Date(), 1))
+      .then(function (result) {
+        expect(result).to.equal(1)
+      })
+  })
+
+  it('should remove a user role by user id', function () {
+    removeUserRoleByUserId.resolves(1)
+    return userRoleService.removeUserRoleByUserId(1)
+      .then(function (result) {
+        expect(result).to.equal(1)
+      })
+  })
+
+  it('should remove a user by username', function () {
+    removeUserByUsername.resolves(1)
+    return userRoleService.removeUserByUsername(STAFF_USER)
+      .then(function (result) {
+        expect(result).to.equal(1)
+      })
   })
 })
