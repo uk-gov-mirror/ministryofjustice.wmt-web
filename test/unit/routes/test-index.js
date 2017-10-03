4a197fcedeb329fadf849f84e48b408d3c3bd8b0
Andrew Wright <a.wright@kainos.com>
2017-10-03 12:42:12 +0100

    323: Ensure no query params are passed to the index route
    If query params are passed to the index route the application should return a
    404.
    
    Also, fix the unit test in the index route by mocking the config object.

--- test/unit/routes/test-index.js
+++ test/unit/routes/test-index.js
@@ -1,32 +1,40 @@
-// const routeHelper = require('../../helpers/routes/route-helper')
-// const supertest = require('supertest')
-// const INDEX_URI = '/'
-// const proxyquire = require('proxyquire')
+const routeHelper = require('../../helpers/routes/route-helper')
+const supertest = require('supertest')
+const INDEX_URI = '/'
+const proxyquire = require('proxyquire')
 
-// describe(`${INDEX_URI}`, function () {
-//   var app
-//   var passport = {}
+describe(`${INDEX_URI}`, function () {
+  var app
+  var passport = {}
+  var mockConfig = {
+    AUTHENTICATION_ENABLED: false
+  }
 
-//   passport.authenticate = function (req, res, next) {
-//     return (strategy) => next()
-//   }
+  passport.authenticate = function (req, res, next) {
+    return (strategy) => next()
+  }
 
-//   beforeEach(function () {
-//     var route = proxyquire(
-//       '../../../app/routes/index', {
-//         'passport': passport
-//       })
-//     app = routeHelper.buildApp(route)
-//   })
+  beforeEach(function () {
+    var route = proxyquire(
+      '../../../app/routes/index', {
+        'passport': passport,
+        '../../config': mockConfig
+      })
+    app = routeHelper.buildApp(route)
+  })
 
-//  // TODO:
-//  // Test is failing, need to mock passport.authenticate.
-//  // The test should be returning 200 but its currently returning 403.
-//   describe(`POST ${INDEX_URI}`, function () {
-//     it('should respond with a 200', function () {
-//       return supertest(app)
-//         .post(INDEX_URI)
-//         .expect(200)
-//     })
-//   })
-// })
+  describe(`POST ${INDEX_URI}`, function () {
+    it('should respond with a 200', function () {
+      return supertest(app)
+        .get(INDEX_URI)
+        .expect(302)
+    })
+  })
+  describe(`GET ${INDEX_URI + '?HPP&TRUE=1/0'}`, function () {
+    it('should respond with an error', function () {
+      return supertest(app)
+        .get(INDEX_URI + '?HPP=TRUE/0')
+        .expect(500)
+    })
+  })
+})
