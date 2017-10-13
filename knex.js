3b67b978d0fecbbbd7b54004fa22c90fe062e606
Andrew Wright <a.wright@kainos.com>
2017-10-13 14:51:28 +0100

    Ensure SET ARITHABORT ON is called on every connection

--- knex.js
+++ knex.js
@@ -1,4 +1,12 @@
-const knexWebSchema = require('knex')(require('./knexfile').web)
+var config = require('./knexfile').web
+
+config.pool.afterCreate = function (conn, done) {
+  conn.batch(["SET ARITHABORT ON"])
+  .then((err, res) => done(err, conn))
+  .catch((err, res) => done(err, conn))
+}
+
+const knexWebSchema = require('knex')(config)
 
 module.exports = {
   web: knexWebSchema
