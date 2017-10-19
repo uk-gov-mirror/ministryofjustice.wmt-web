51fb80456c26cd2fca2df2cb2f859934834d5153
Andrew Wright <a.wright@kainos.com>
2017-10-19 11:31:51 +0100

    WIP: Add with(noexpand) hint to the capacity query

--- app/services/data/get-capacity-breakdown.js
+++ app/services/data/get-capacity-breakdown.js
@@ -5,25 +5,30 @@ module.exports = function (id, type) {
   var orgUnit = orgUnitFinder('name', type)
 
   var table = orgUnit.capacityBreakdownView
-  var whereObject = {}
+  var whereString = ""
 
-  if (id !== undefined) {
-    whereObject.id = id
+  if (id !== undefined && (NaN !== parseInt(id, 10))) {
+    whereString = " WHERE id = " + id
   }
 
-  return knex(table)
-    .where(whereObject)
-    .select('total_points AS totalPoints',
-            'available_points AS availablePoints',
-            'reduction_hours AS reductionHours',
-            'link_id AS linkId',
-            'name',
-            'grade_code AS grade',
-            'total_cases AS totalCases',
-            'cms_adjustment_points AS cmsAdjustmentPoints',
-            'gs_adjustment_points AS gsAdjustmentPoints',
-            'arms_total_cases AS armsTotalCases',
-            'contracted_hours AS contractedHours')
+  var selectList = [
+    'total_points AS totalPoints',
+    'available_points AS availablePoints',
+    'reduction_hours AS reductionHours',
+    'link_id AS linkId',
+    'name',
+    'grade_code AS grade',
+    'total_cases AS totalCases',
+    'cms_adjustment_points AS cmsAdjustmentPoints',
+    'gs_adjustment_points AS gsAdjustmentPoints',
+    'arms_total_cases AS armsTotalCases',
+    'contracted_hours AS contractedHours'
+  ]
+
+  return knex.schema.raw("SELECT " + selectList.join(', ') +
+          " FROM " + table +
+          " WITH (NOEXPAND) " +
+          whereString)
     .then(function (results) {
       return results
     })
