e084efdc4800136cc5bb2e2e2f8ec7db501bbf7b
Andrew Wright <a.wright@kainos.com>
2017-10-25 16:48:41 +0100

    Call caseload views with noexpand hint.
    Also, now the views take the total cases from the tiers table, instead of the
    workload table - meaning the integration tests had to be updated.
    
    Finally, now that all the caseload views are indexed, these indexes need to be
    maintained when an insert occurs this means the timeout of requests need to be
    extended.

--- app/services/data/get-caseload.js
+++ app/services/data/get-caseload.js
@@ -1,14 +1,16 @@
 const knex = require('../../../knex').web
 const orgUnitFinder = require('../helpers/org-unit-finder')
+const ORGANISATION_UNIT = require('../../constants/organisation-unit')
 
 module.exports = function (id, type) {
   var orgUnit = orgUnitFinder('name', type)
   var table = orgUnit.caseloadView
 
-  var selectColumns = [
+  var selectList = [
     'link_id AS linkId',
-    'name',
     'grade_code AS grade',
+    'total_cases AS totalCases',
+    'location AS caseType',
     'untiered',
     'd2',
     'd1',
@@ -16,15 +18,27 @@ module.exports = function (id, type) {
     'c1',
     'b2',
     'b1',
-    'a',
-    'total_cases AS totalCases',
-    'case_type AS caseType'
+    'a'
   ]
 
-  return knex(table)
-    .where('id', id)
-    .select(selectColumns)
-    .then(function (results) {
-      return results
-    })
+  var requiresWorkloadOwnerName = (type === ORGANISATION_UNIT.TEAM.name)
+
+  if (requiresWorkloadOwnerName) {
+    selectList.push('CONCAT(forename, \' \', surname) AS name')
+  } else {
+    selectList.push('name')
+  }
+
+  var whereString = ''
+
+  if (id !== undefined && (!isNaN(parseInt(id, 10)))) {
+    whereString += ' WHERE id = ' + id
+  }
+
+  var noExpandHint = ' WITH (NOEXPAND)'
+
+  return knex.schema.raw('SELECT ' + selectList.join(', ') +
+      ' FROM ' + table +
+      noExpandHint +
+      whereString)
 }
