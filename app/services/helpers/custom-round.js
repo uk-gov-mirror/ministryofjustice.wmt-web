ae0dc3565687e7452ba02a7cfecbfc017f33360e
davidmck <d.mckinney@kainos.com>
2017-10-25 11:48:34 +0100

    Modified custom round to make rounding .6 as accurate as possible

--- app/services/helpers/custom-round.js
+++ app/services/helpers/custom-round.js
@@ -2,7 +2,7 @@ module.exports.customRound = function (number) {
   if (number || number === 0) {
     if ((number % 1) !== 0) {
       let decimal = number % 1
-      if (decimal < 0.5999999999999943 && decimal >= 0.5) {
+      if (decimal < 0.59999999 && decimal >= 0.5) {
         number = Math.floor(number)
       } else {
         number = Math.round(number)
