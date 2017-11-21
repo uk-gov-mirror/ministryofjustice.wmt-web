90b0450e92c0736bc9df9a9e7f007dab86eb21ca
chloemc <c.mcateer@kainos.com>
2017-11-21 09:49:24 +0000

    route added to link the archive data view

--- app/routes/archive.js
+++ app/routes/archive.js
@@ -1,27 +1,32 @@
-const getArchiveDate = require('../services/__get_archive.js')
-const ValidationError = require('../services/errors/validation-error')
+const getArchiveData = require('../services/data/__get_archive.js')
 const authorisation = require('../authorisation')
-const getOrganisationUnit = require('../services/helpers/org-unit-finder')
+const messages = require('../constants/messages')
+const roles = require('../constants/user-roles')
+const Unauthorized = require('../services/errors/authentication-error').Unauthorized
+const Forbidden = require('../services/errors/authentication-error').Forbidden
 
 module.exports = function (router) {
+  router.get('/archive-data', function (req, res, next) {
+    try {
+      authorisation.assertUserAuthenticated(req)
+      authorisation.hasRole(req, [roles.SYSTEM_ADMIN, roles.DATA_ADMIN])
+    } catch (error) {
+      if (error instanceof Unauthorized) {
+        return res.status(error.statusCode).redirect(error.redirect)
+      } else if (error instanceof Forbidden) {
+        return res.status(error.statusCode).render(error.redirect, {
+          heading: messages.ACCESS_DENIED,
+          message: messages.ADMIN_ROLES_REQUIRED
+        })
+      }
+    }
 
-    router.get('/admin/archive-data', function (req, res) {
-        try {
-            authorisation.assertUserAuthenticated(req)
-            authorisation.hasRole(req, [roles.SYSTEM_ADMIN, roles.DATA_ADMIN])
-          } catch (error) {
-            if (error instanceof Unauthorized) {
-              return res.status(error.statusCode).redirect(error.redirect)
-            } else if (error instanceof Forbidden) {
-              return res.status(error.statusCode).render(error.redirect, {
-                heading: messages.ACCESS_DENIED,
-                message: messages.ADMIN_ROLES_REQUIRED
-              })
-            }
-          }
+    var authorisedUserRole = authorisation.getAuthorisedUserRole(req)
+
+    return res.render('archive-data', {
+      title: 'Archive Data',
+      userRole: authorisedUserRole.userRole, // used by proposition-link for the admin role
+      noAuth: authorisedUserRole.noAuth  // used by proposition-link for the admin role
     })
-    var success = req.query.success
-    .then(function (result) {
-        return res.render('archive-data')
-    })
+})
 }
