0f3c08704feda9aba264f8acf38bf4b0b5913998
chloemc <c.mcateer@kainos.com>
2017-11-22 09:18:41 +0000

    Unit test for archive route

---
+++ test/unit/routes/test-archive-data.js
@@ -0,0 +1,37 @@
+const routeHelper = require('../../helpers/routes/route-helper')
+const supertest = require('supertest')
+const proxyquire = require('proxyquire')
+const sinon = require('sinon')
+const config = require('../../../config')
+require('sinon-bluebird')
+
+const ARCHIVE_DATA_URL = '/archive-data'
+
+var app
+var route
+var userRoleService
+var authorisationService
+var hasRoleStub = sinon.stub()
+
+var initaliseApp = function () {
+    userRoleService = sinon.stub()
+    authorisationService = {
+      assertUserAuthenticated: sinon.stub(),
+      hasRole: hasRoleStub
+    }
+    route = proxyquire('../../../app/routes/archive', {
+        '../services/user-role-service': userRoleService,
+        '../authorisation': authorisationService
+      })
+      app = routeHelper.buildApp(route)
+    }
+
+before(function () {
+    initaliseApp()
+})
+
+describe('archive route', function () {
+    it('should respond with 200 when archieve data is called', function () {
+      return supertest(app).get(ARCHIVE_DATA_URL).expect(200)
+    })
+  })
