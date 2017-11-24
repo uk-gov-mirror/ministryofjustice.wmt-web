c0926be612b6543b75bbcd89a2102d68e778f96e
chloemc <c.mcateer@kainos.com>
2017-11-24 12:02:29 +0000

    defulat 100 rows for archive table

--- app/assets/javascripts/archive-data.js
+++ app/assets/javascripts/archive-data.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $('.js-data-table').dataTable({
       paging: true,
+      "pageLength": 100,
       searching: true,
       info: false,
       "aaSorting": [],
