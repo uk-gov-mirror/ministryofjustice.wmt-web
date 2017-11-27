8e62b8967b783e663b259331fef3ea97a34b2d2b
chloemc <c.mcateer@kainos.com>
2017-11-27 13:05:46 +0000

    run standard --fix

--- app/routes/archive-date.js
+++ app/routes/archive-date.js
@@ -5,27 +5,27 @@ const Unauthorized = require('../services/errors/authentication-error').Unauthor
 const Forbidden = require('../services/errors/authentication-error').Forbidden
 
 module.exports = function (router) {
-    router.get('/archive-date', function (req, res, next) {
-      try {
-        authorisation.assertUserAuthenticated(req)
-        authorisation.hasRole(req, [roles.DATA_ADMIN])
-      } catch (error) {
-        if (error instanceof Unauthorized) {
-          return res.status(error.statusCode).redirect(error.redirect)
-        } else if (error instanceof Forbidden) {
-          return res.status(error.statusCode).render(error.redirect, {
-            heading: messages.ACCESS_DENIED,
-            message: messages.ADMIN_ROLES_REQUIRED
-          })
-        }
-      }
-  
-      var authorisedUserRole = authorisation.getAuthorisedUserRole(req)
-  
-        return res.render('archive-date', {
-          title: 'Archive date',
-          userRole: authorisedUserRole.userRole, // used by proposition-link for the admin role
-          noAuth: authorisedUserRole.noAuth  // used by proposition-link for the admin role
+  router.get('/archive-date', function (req, res, next) {
+    try {
+      authorisation.assertUserAuthenticated(req)
+      authorisation.hasRole(req, [roles.DATA_ADMIN])
+    } catch (error) {
+      if (error instanceof Unauthorized) {
+        return res.status(error.statusCode).redirect(error.redirect)
+      } else if (error instanceof Forbidden) {
+        return res.status(error.statusCode).render(error.redirect, {
+          heading: messages.ACCESS_DENIED,
+          message: messages.ADMIN_ROLES_REQUIRED
         })
-      })
-  }
+      }
+    }
+
+    var authorisedUserRole = authorisation.getAuthorisedUserRole(req)
+
+    return res.render('archive-date', {
+      title: 'Archive date',
+      userRole: authorisedUserRole.userRole, // used by proposition-link for the admin role
+      noAuth: authorisedUserRole.noAuth  // used by proposition-link for the admin role
+    })
+  })
+}
