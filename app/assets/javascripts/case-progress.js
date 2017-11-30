53a6e5277a63c65a004e82b0a71d4ae8fcef8260
chloemc <c.mcateer@kainos.com>
2017-11-30 15:40:30 +0000

    small change to indentation

--- app/assets/javascripts/case-progress.js
+++ app/assets/javascripts/case-progress.js
@@ -18,12 +18,12 @@ caseProgress.forEach(function (caseInfo) {
 
 function _toConsumableArray(arr) { 
     if (Array.isArray(arr)) {
-         for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { 
-             arr2[i] = arr[i]; } return arr2; 
-            } else { 
-                return Array.from(arr); 
-            } 
-        }
+    for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { 
+        arr2[i] = arr[i]; } return arr2; 
+    } else { 
+        return Array.from(arr); 
+    } 
+}
 maxLabelLength = Math.max.apply(Math, _toConsumableArray(namesY.map(function (name) {
   return name.length;
 })));
