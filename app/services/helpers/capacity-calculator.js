ca9441850cfa7279d504a4d79f8f9cc206053f2a
David McKinney <d.mckinney@kainos.com>
2017-12-07 13:22:26 +0000

    Prevent divide by zero errors when calculating capacity and reductions percentages for capacity graph

--- app/services/helpers/capacity-calculator.js
+++ app/services/helpers/capacity-calculator.js
@@ -2,8 +2,18 @@
 module.exports.calculate = function (workloadReports) {
   var capacities = []
   workloadReports.forEach(function (report) {
-    var capacityPercentage = (report.total_points / report.available_points) * 100
-    var reductionPercentage = (report.reduction_hours / report.contracted_hours) * 100
+    var capacityPercentage
+    var reductionPercentage
+    if(report.available_points === 0) {
+      capacityPercentage = 0
+    } else {
+      capacityPercentage = (report.total_points / report.available_points) * 100
+    }
+    if(report.contracted_hours === 0) {
+      reductionPercentage = 0
+    } else {
+      reductionPercentage = (report.reduction_hours / report.contracted_hours) * 100
+    }
     capacities.push({
       workload_report_date: report.effective_from,
       capacity_percentage: capacityPercentage,
