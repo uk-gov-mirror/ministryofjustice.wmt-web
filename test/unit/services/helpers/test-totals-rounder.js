89746ac9a96f81ddf8a50f6a2e25e9e9a3c83c0e
kevinfox <kevinfox@kainos.com>
2018-01-31 10:33:03 +0000

    Fixed space before comma

--- test/unit/services/helpers/test-totals-rounder.js
+++ test/unit/services/helpers/test-totals-rounder.js
@@ -3,13 +3,13 @@ const totalsRounder = require('../../../../app/services/helpers/totals-rounder')
 
 describe('services/helpers/totals-rounder', function () {
   it('should round total contrcated hours to 1 decimal place', function () {
-    var totals = { name: 'Total / Average', totalCapacityPercentage: 0, totalPoints: 0, totalAvailablePoints: 0, totalContractedHours: 118416.70000000001	, totalReduction: 77371.40000000001 }
+    var totals = { name: 'Total / Average', totalCapacityPercentage: 0, totalPoints: 0, totalAvailablePoints: 0, totalContractedHours: 118416.70000000001, totalReduction: 77371.40000000001 }
     totals = totalsRounder(totals)
     expect(totals.totalContractedHours.to.eql(118416.7))
   })
 
   it('should round total reduction hours to 1 decimal place', function () {
-    var totals = { name: 'Total / Average', totalCapacityPercentage: 0, totalPoints: 0, totalAvailablePoints: 0, totalContractedHours: 118416.70000000001	, totalReduction: 77371.40000000001 }
+    var totals = { name: 'Total / Average', totalCapacityPercentage: 0, totalPoints: 0, totalAvailablePoints: 0, totalContractedHours: 118416.70000000001, totalReduction: 77371.40000000001 }
     totals = totalsRounder(totals)
     expect(totals.totalReduction.to.eql(77371.4))
   })
