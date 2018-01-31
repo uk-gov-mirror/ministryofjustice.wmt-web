ca2663ed16fa79f6ff0da0b21fa038ec0085d69f
kevinfox <kevinfox@kainos.com>
2018-01-31 09:38:33 +0000

    Changing tab size to 2 spaces

--- app/services/helpers/totals-rounder.js
+++ app/services/helpers/totals-rounder.js
@@ -1,5 +1,5 @@
 module.exports = function (totals) {
-    totals.totalContractedHours = totals.totalContractedHours.toFixed(1)
-    totals.totalReduction = totals.totalReduction.toFixed(1)
-    return totals
+  totals.totalContractedHours = totals.totalContractedHours.toFixed(1)
+  totals.totalReduction = totals.totalReduction.toFixed(1)
+  return totals
 }
