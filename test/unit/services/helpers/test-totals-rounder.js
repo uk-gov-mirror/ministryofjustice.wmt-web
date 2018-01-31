53903a57463987918d959f360eb7915177cd9687
kevinfox <kevinfox@kainos.com>
2018-01-31 11:14:47 +0000

    Fixed the expect statements and removed now redundant logger references

--- test/unit/services/helpers/test-totals-rounder.js
+++ test/unit/services/helpers/test-totals-rounder.js
@@ -1,19 +1,16 @@
 const expect = require('chai').expect
 const totalsRounder = require('../../../../app/services/helpers/totals-rounder')
-const logger = require('../../../../app/logger')
 
 describe('services/helpers/totals-rounder', function () {
   it('should round total contrcated hours to 1 decimal place', function () {
     var totals = { name: 'Total / Average', totalCapacityPercentage: 0, totalPoints: 0, totalAvailablePoints: 0, totalContractedHours: 118416.70000000001, totalReduction: 77371.40000000001 }
     totals = totalsRounder(totals)
-    expect(totals.totalContractedHours.to.eql(118416.7))
+    expect(totals.totalContractedHours).to.eql(118416.7)
   })
 
   it('should round total reduction hours to 1 decimal place', function () {
     var totals = { name: 'Total / Average', totalCapacityPercentage: 0, totalPoints: 0, totalAvailablePoints: 0, totalContractedHours: 118416.70000000001, totalReduction: 77371.40000000001 }
-    logger.info(totals)
     totals = totalsRounder(totals)
-    logger.info(totals)
-    expect(totals.totalReduction.to.eql(77371.4))
+    expect(totals.totalReduction).to.eql(77371.4)
   })
 })
