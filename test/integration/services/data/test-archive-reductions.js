5ff0904cd0e5c4765d54b76c2d6a3532b1fcd8a0
kevinfox <kevinfox@kainos.com>
2018-05-03 14:47:27 +0100

    WMT0043: Added Integration test for archive reductions

---
+++ test/integration/services/data/test-archive-reductions.js
@@ -0,0 +1,27 @@
+const expect = require('chai').expect
+
+const getArchivedReductions = require('../../../../app/services/data/get-reduction-archive')
+
+var expectedResult = {
+  omName: 'A.N. Offender Manager KNTK042PSO',
+  hoursReduced: 18.8,
+  comments: '67A1623',
+  lastUpdatedDate: new Date('2014-10-24T10:14:14.093Z'),
+  reductionAddedBy: 'A.N. Offender Manager 5'
+}
+
+var archiveReductionData
+
+describe('services/data/get-daily-archive', function () {
+  before(function () {
+    return getArchivedReductions().then(function (results) {
+      archiveReductionData = results
+    })
+  })
+  it('should retrieve all 5 columns for archive data', function () {
+    expect(archiveReductionData[0].omName).to.eql(expectedResult.omName)
+    expect(archiveReductionData[0].reductionAddedBy).to.eql(expectedResult.reductionAddedBy)
+    expect(archiveReductionData[0].comments).to.eql(expectedResult.comments)
+    expect(archiveReductionData[0].reductionAddedBy).to.eql(expectedResult.reductionAddedBy)
+  })
+})
