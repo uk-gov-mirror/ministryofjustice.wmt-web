66ffe536ef0a747c55f579feba64ec977622c147
kevinfox <kevinfox@kainos.com>
2019-01-11 16:01:11 +0000

    WMT0081: Changed the way sorting takes place on the daily archive data view. Implemented sorting by date

--- app/assets/javascripts/archive-data.js
+++ app/assets/javascripts/archive-data.js
@@ -1,12 +1,29 @@
 //var data
 
+$.fn.dataTable.moment = function ( format, locale ) {
+  var types = $.fn.dataTable.ext.type;
+
+  // Add type detection
+  types.detect.unshift( function ( d ) {
+      return moment( d, format, locale, true ).isValid() ?
+          'moment-'+format :
+          null;
+  } );
+
+  // Add sorting method - use an integer for the sorting
+  types.order[ 'moment-'+format+'-pre' ] = function ( d ) {
+      return moment( d, format, locale, true ).unix();
+  };
+};
+
 $(document).ready(function() {
+    $.fn.dataTable.moment( 'DD-MM-YYYY');
     $('.js-data-table').dataTable({
       paging: true,
       "pageLength": 100,
       searching: true,
       info: false,
-      order: [[1, 'asc']],
+      order: [[0, 'asc']],
       "aaSorting": [],
       columnDefs: [{
         targets: [],
