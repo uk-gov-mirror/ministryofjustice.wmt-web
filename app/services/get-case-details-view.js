dd544f2079dd46a16d295732c02feea3a9edc19e
kevinfox <kevinfox@kainos.com>
2019-01-14 15:10:41 +0000

    WMT0092: Adding code to map grade codes. Correcting case details column headings

--- app/services/get-case-details-view.js
+++ app/services/get-case-details-view.js
@@ -13,9 +13,66 @@ module.exports = function (id, organisationLevel) {
   if (organisationalUnitType === organisationConstant.OFFENDER_MANAGER) {
     return getCaseDetails(id)
       .then(function (result) {
+        result = formatTier(result)
         return result
       })
   } else {
     return Promise.resolve(result)
   }
+}
+
+var formatTier = function (results) {
+  results.forEach(function (result) {
+    switch(result.tierCode) {
+      case '0':
+        result.tierCode = 'Untiered'
+        break
+      case '1':
+        result.tierCode = 'D2'
+        break
+      case '2':
+        result.tierCode = 'D1'
+        break
+      case '3':
+        result.tierCode = 'C2'
+        break
+      case '4':
+        result.tierCode = 'C2'
+        break
+      case '5':
+        result.tierCode = 'B2'
+        break
+      case '6':
+        result.tierCode = 'B1'
+        break
+      case '7':
+        result.tierCode = 'A'
+        break
+      case 0:
+        result.tierCode = 'Untiered'
+        break
+      case 1:
+        result.tierCode = 'D2'
+        break
+      case 2:
+        result.tierCode = 'D1'
+        break
+      case 3:
+        result.tierCode = 'C2'
+        break
+      case 4:
+        result.tierCode = 'C2'
+        break
+      case 5:
+        result.tierCode = 'B2'
+        break
+      case 6:
+        result.tierCode = 'B1'
+        break
+      case 7:
+        result.tierCode = 'A'
+        break
+    }
+  })
+  return results
 }
