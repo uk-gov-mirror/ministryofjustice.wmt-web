205b5169ef9f2e1e63900b0b055fadd14c563dfd
stephenrogan <stephenr@kainos.com>
2019-01-18 14:02:54 +0000

    WMT-0098: Finished Express routes, HTML view and Data service

--- app/routes/search.js
+++ app/routes/search.js
@@ -1,6 +1,15 @@
+const offenderSearch = require('../services/data/search-for-offender')
 
 module.exports = function (router) {
-    router.get('/search', function (req, res, next) {
+    router.get('/officer-search', function (req, res, next) {
         return res.render('search-for-officer', {})
+    })
+
+    router.post('/officer-search/search', function(req, res, next){
+        return offenderSearch(req.body.surnameBox).then(function(result){
+            res.render('search-for-officer', {
+                 results: result
+            })
+        })
     })
 }
