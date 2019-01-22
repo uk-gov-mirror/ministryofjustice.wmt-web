72870e5982057987e4ed842d9321bf9e9a76fdb5
stephenrogan <stephenr@kainos.com>
2019-01-22 16:31:33 +0000

    WMT0098: Adding Validation

--- app/routes/search.js
+++ app/routes/search.js
@@ -1,11 +1,47 @@
 const offenderSearch = require('../services/data/search-for-offender')
+const ValidationError = require('../services/errors/validation-error')
+const FieldSetValidator = require('../services/validators/fieldset-validator')
+const ErrorHandler = require('../services/validators/error-handler')
+const authorisation = require('../authorisation')
+const Unauthorized = require('../services/errors/authentication-error').Unauthorized
+const renderResults = require('../helpers/render-results')
+const title = 'Search for an Offender Manager'
+const viewTemplate = 'search-for-officer'
 
 module.exports = function (router) {
     router.get('/officer-search', function (req, res, next) {
+        try {
+            authorisation.assertUserAuthenticated(req)
+          } catch (error) {
+            if (error instanceof Unauthorized) {
+              return res.status(error.statusCode).redirect(error.redirect)
+            }
+        }
         return res.render('search-for-officer', {})
     })
 
     router.post('/officer-search/search', function(req, res, next){
+        try {
+            authorisation.assertUserAuthenticated(req)
+          } catch (error) {
+            if (error instanceof Unauthorized) {
+              return res.status(error.statusCode).redirect(error.redirect)
+            }
+        }
+        var authorisedUserRole = authorisation.getAuthorisedUserRole(req)
+        var errors
+        try {
+            isValid(req.body.surnameBox)
+        } catch (error) {
+            if (error instanceof ValidationError) {
+                errors = error.validationErrors
+              } else {
+                throw error
+            }
+        }
+        if (errors) {
+            return renderResults(viewTemplate, title, res, errors, null, authorisedUserRole)
+        }
         return offenderSearch(req.body.surnameBox).then(function(result){
             res.render('search-for-officer', {
                  results: result
@@ -13,3 +49,16 @@ module.exports = function (router) {
         })
     })
 }
+
+var isValid = function (surname) {
+    var errors = ErrorHandler()
+
+    this.search = FieldSetValidator(surname, 'search', errors)
+      .isRequired()
+
+    var validationErrors = errors.get()
+
+    if (validationErrors) {
+      throw new ValidationError(validationErrors)
+    }
+  }
