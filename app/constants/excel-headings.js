aae34ef7be911d12872bef4c990a9ffb5c6fec9f
kevinfox <kevinfox@kainos.com>
2019-01-25 18:31:33 +0000

    WMT0095: Added scenario route and logic to create scenario template

---
+++ app/constants/excel-headings.js
@@ -0,0 +1,69 @@
+const caseHeaders = [
+  'Com Untiered',
+  'Com D2',
+  'Com D1',
+  'Com C2',
+  'Com C1',
+  'Com B2',
+  'Com B1',
+  'Com A',
+  'Lic Untiered',
+  'Lic D2',
+  'Lic D1',
+  'Lic C2',
+  'Lic C1',
+  'Lic B2',
+  'Lic B1',
+  'Lic A',
+  'Cus Untiered',
+  'Cus D2',
+  'Cus D1',
+  'Cus C2',
+  'Cus C1',
+  'Cus B2',
+  'Cus B1',
+  'Cus A'
+]
+const nameHeaders = [
+  'Name',
+  'Grade',
+  'Total Cases',
+  'Nominal Target',
+  'Contracted Hours',
+  'Default Contracted Hours',
+  'Reduction Hours',
+  'CMS Points',
+  'CMS %',
+  'GS Points',
+  'GS %', 'SDR Points',
+  'FDR Points',
+  'Parom Points',
+  'ARMS Comm Points',
+  'ARMS Licence Points',
+  'Total Caseload Points',
+  'Overall Total Points',
+  'Available Points',
+  'Remaining Points',
+  'Current % Capacity'
+]
+const reportHeaders = [
+  'SDR',
+  'FDR (SDR Conversion)',
+  'Parom',
+  'ARMS Comm',
+  'ARMS Licence'
+]
+const caseTypeHeaders = [
+  'Active Cases',
+  'Warrants (0%)',
+  'UPW (100%)',
+  'Overdue Terminations (0%)'
+]
+
+module.exports.caseHeaders = caseHeaders
+
+module.exports.nameHeaders = nameHeaders
+
+module.exports.reportHeaders = reportHeaders
+
+module.exports.caseTypeHeaders = caseTypeHeaders
---
+++ app/routes/scenario.js
@@ -0,0 +1,22 @@
+const authorisation = require('../authorisation')
+const Unauthorized = require('../services/errors/authentication-error').Unauthorized
+const workloadPointsService = require('../services/data/get-workload-points-export')
+const getExportXlsx = require('../services/get-export-xlsx')
+
+module.exports = function (router) {
+  router.get('/scenario', function (req, res, next) {
+    try {
+      authorisation.assertUserAuthenticated(req)
+    } catch (error) {
+      if (error instanceof Unauthorized) {
+        return res.status(error.statusCode).redirect(error.redirect)
+      }
+    }
+    return workloadPointsService(false).then(function (result) {
+      return workloadPointsService(true).then(function (t2aResult) {
+        wb = getExportXlsx(result, t2aResult)
+        wb.write('ExcelFile.xlsx', res);
+      })
+    })
+  })
+}
