486389cd6bdbb5f402238cadae8a75005a2c3d16
stephenrogan <stephenr@kainos.com>
2019-01-29 11:43:41 +0000

    WMT0098: Fixing coding standards

--- app/routes/search.js
+++ app/routes/search.js
@@ -1,37 +1,31 @@
 const offenderSearch = require('../services/data/search-for-offender')
-const ValidationError = require('../services/errors/validation-error')
-const FieldSetValidator = require('../services/validators/fieldset-validator')
-const ErrorHandler = require('../services/validators/error-handler')
 const authorisation = require('../authorisation')
 const Unauthorized = require('../services/errors/authentication-error').Unauthorized
-const renderResults = require('../helpers/render-results')
-const title = 'Search for an Offender Manager'
-const viewTemplate = 'search-for-officer'
 
 module.exports = function (router) {
-    router.get('/officer-search', function (req, res, next) {
-        try {
-            authorisation.assertUserAuthenticated(req)
-          } catch (error) {
-            if (error instanceof Unauthorized) {
-              return res.status(error.statusCode).redirect(error.redirect)
-            }
-        }
-        return res.render('search-for-officer', {})
-    })
+  router.get('/officer-search', function (req, res, next) {
+    try {
+      authorisation.assertUserAuthenticated(req)
+    } catch (error) {
+      if (error instanceof Unauthorized) {
+        return res.status(error.statusCode).redirect(error.redirect)
+      }
+    }
+    return res.render('search-for-officer', {})
+  })
 
-    router.post('/officer-search/search', function(req, res, next){
-        try {
-            authorisation.assertUserAuthenticated(req)
-          } catch (error) {
-            if (error instanceof Unauthorized) {
-              return res.status(error.statusCode).redirect(error.redirect)
-            }
-        }
-        return offenderSearch(req.body.surnameBox).then(function(result){
-            res.render('search-for-officer', {
-                 results: result
-            })
-        })
+  router.post('/officer-search/search', function (req, res, next) {
+    try {
+      authorisation.assertUserAuthenticated(req)
+    } catch (error) {
+      if (error instanceof Unauthorized) {
+        return res.status(error.statusCode).redirect(error.redirect)
+      }
+    }
+    return offenderSearch(req.body.surnameBox).then(function (result) {
+      res.render('search-for-officer', {
+        results: result
+      })
     })
+  })
 }
--- app/services/data/search-for-offender.js
+++ app/services/data/search-for-offender.js
@@ -2,13 +2,13 @@ const knex = require('../../../knex').web
 const log = require('../../logger')
 
 module.exports = function (surname) {
-    var columns = [
-        'forename',
-        'surname',
-        'team',
-        'ldu',
-        'region'
-    ]
-    log.info(knex('offender_manager_search_view').columns(columns).where('surname', surname).toString())
-    return knex('offender_manager_search_view').columns(columns).where('surname', surname)
+  var columns = [
+    'forename',
+    'surname',
+    'team',
+    'ldu',
+    'region'
+  ]
+  log.info(knex('offender_manager_search_view').columns(columns).where('surname', surname).toString())
+  return knex('offender_manager_search_view').columns(columns).where('surname', surname)
 }
