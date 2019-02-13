4f45463a3f3bafbd165bf86a128aee999b223d40
kevinfox <kevinfox@kainos.com>
2019-02-13 20:41:46 +0000

    WMT0095: Applied freeze panes, header colouring, formula corrections. Updated unit tests

--- app/services/excel/input-scenario-case-data.js
+++ app/services/excel/input-scenario-case-data.js
@@ -12,7 +12,7 @@ var inputCaseData = function (ws, cases, typeTierGroupLength, tiersPerType) {
   var columnStart = 22
 
   for (var c of cases) {
-    inputFormulas(ws, rowStart)
+    inputMainBodyFormulas(ws, rowStart)
     inputOffenderManagerData(ws, c, rowStart)
     for (var i = 0; i < tiersPerType; i++) {
       var casesForThisTier = c.communityCaseNumbers.filter(thisCase => thisCase.tier === i)
@@ -52,33 +52,33 @@ var inputCaseData = function (ws, cases, typeTierGroupLength, tiersPerType) {
       columnStart = columnStart + typeTierGroupLength
     }
     // reports to go here
-    ws.cell(rowStart, columnStart).number(c.sdrTotal)
-    ws.cell(rowStart, columnStart + 1).number(c.sdrConversionsTotal)
-    ws.cell(rowStart, columnStart + 2).number(c.paromsTotal)
-    ws.cell(rowStart, columnStart + 3).number(c.armsCommunity)
-    ws.cell(rowStart, columnStart + 4).number(c.armsLicense)
+    ws.cell(rowStart, columnStart).number(c.sdrTotal).style(styles.caseStyle)
+    ws.cell(rowStart, columnStart + 1).number(c.sdrConversionsTotal).style(styles.caseStyle)
+    ws.cell(rowStart, columnStart + 2).number(c.paromsTotal).style(styles.caseStyle)
+    ws.cell(rowStart, columnStart + 3).number(c.armsCommunity).style(styles.caseStyle)
+    ws.cell(rowStart, columnStart + 4).number(c.armsLicense).style(styles.caseStyle)
     columnStart = 22
     rowStart = rowStart + 1
   }
-  inputTotals(ws, rowStart)
+  inputBottomTotals(ws, rowStart)
 }
 
 // Input Offender Manager Name, Grade to the left side of the produced spreadsheet
 var inputOffenderManagerData = function (ws, offenderManager, row) {
-  ws.cell(row, 1).string(offenderManager.name)
-  ws.cell(row, 2).string(offenderManager.grade)
-  ws.cell(row, 4).number(offenderManager.nominalTarget)
-  ws.cell(row, 5).number(offenderManager.contractedHours)
-  ws.cell(row, 6).number(offenderManager.defaultContractedHours)
-  ws.cell(row, 7).number(offenderManager.reductionHours)
-  ws.cell(row, 8).number(offenderManager.cms)
-  ws.cell(row, 10).number(offenderManager.gs)
+  ws.cell(row, 1).string(offenderManager.name).style(styles.caseStyle)
+  ws.cell(row, 2).string(offenderManager.grade).style(styles.caseStyle)
+  ws.cell(row, 4).number(offenderManager.nominalTarget).style(styles.caseStyle)
+  ws.cell(row, 5).number(offenderManager.contractedHours).style(styles.caseStyle)
+  ws.cell(row, 6).number(offenderManager.defaultContractedHours).style(styles.caseStyle)
+  ws.cell(row, 7).number(offenderManager.reductionHours).style(styles.caseStyle)
+  ws.cell(row, 8).number(offenderManager.cms).style(styles.caseStyle)
+  ws.cell(row, 10).number(offenderManager.gs).style(styles.caseStyle)
 }
 
 var totalPointsFormula = function (row) {
   // return '=SUM(V4*V' + row + ',W4*W' + row + ',X4*X' + row + ',Y4*Y' + row + ',Z4*Z' + row + ',AA4*AA' + row + ',AB4*AB' + row + ',AC4*AC' + row + ',AD4*AD' + row + ',AE4*AE' + row + ',AF4*AF' + row + ',AG4*AG' + row + ',AH4*AH' + row + ',AI4*AI' + row + ',AJ4*AJ' + row + ',AK4*AK' + row + ',AL4*AL' + row + ',AM4*AM' + row + ',AN4*AN' + row + ',AO4*AO' + row + ',AP4*AP' + row + ',AQ4*AQ' + row + ',AR4*AR' + row + ',AS4*AS' + row + ',AT4*AT' + row + ',AU4*AU' + row + ',AV4*AV' + row + ',AW4*AW' + row + ',AX4*AX' + row + ',AY4*AY' + row + ',AZ4*AZ' + row + ',BA4*BA' + row + ',BB4*BB' + row + ',BC4*BC' + row + ',BD4*BD' + row + ',BE4*BE' + row + ',BF4*BF' + row + ',BG4*BG' + row + ',BH4*BH' + row + ',BI4*BI' + row + ',BJ4*BJ' + row + ',BK4*BK' + row + ',BL4*BL' + row + ',BM4*BM' + row + ',BN4*BN' + row + ',BO4*BO' + row + ',BP4*BP' + row + ',BQ4*BQ' + row + ',BR4*BR' + row + ',BS4*BS' + row + ',BT4*BT' + row + ',BU4*BU' + row + ',BV4*BV' + row + ',BW4*BW' + row + ',BX4*BX' + ',BY4*BY' + row + ',BZ4*BZ' + row + ',CA4*CA' + row + ',CB4*CB' + row + ',CC4*CC' + row + ',CD4*CD' + row + ',CE4*CE' + row + ',CF4*CF' + row + ',CG4*CG' + row + ',CH4*CH' + row + ',CI4*CI' + row + ',CJ4*CJ' + row + ',CK4*CK' + row + ',CL4*CL' + row + ',CM4*CM' + row + ',CN4*CN' + row + ',CO4*CO' + row + ',CP4*CP' + row + ',CQ4*CQ' + row + ',CR4*CR' + row + ',CS4*CS' + row + ',CT4*CT' + row + ',CU4*CU' + row + ',CV4*CV' + row + ',CW4*CW' + row + ',CX4*CX' + row + ',CY4*CY' + row + ',CZ4*CZ' + row + ',DA4*DA' + row + ',DB4*DB' + row + ',DC4*DC' + row + ',DD4*DD' + row + ',DE4*DE' + row + ',DF4*DF' + row + ',DG4*DG' + row + ',DH4*DH' + row + ',DI4*DI' + row + ',DJ4*DJ' + row + ',DK4*DK' + row + ',DL4*DL' + row + ',DM4*DM' + row + ',DN4*DN' + row + ',DO4*DO' + row + ',DP4*DP' + row + ',DQ4*DQ' + row + ',DR4*DR' + row + ',DS4*DS' + row + ',DT4*DT' + row + ',DU4*DU' + row + ',DV4*DV' + row + ',DW4*DW' + row + ',DX4*DX' + row + ',DY4*DY' + row + ',DZ4*DZ' + row + ',EA4*EA' + row + ',EB4*EB' + row + ',EC4*EC' + row + ',ED4*ED' + row + ',EE4*EE' + row + ',EF4*EF' + row + ',EG4*EG' + row + ',EH4*EH' + row + ',EI4*EI' + row + ',EJ4*EJ' + row + ',EK4*EK' + row + ',EL4*EL' + row + ',EM4*EM' + row + ',EN4*EN' + row + ',EO4*EO' + row + ',EP4*EP' + row + ',EQ4*EQ' + row + ',ER4*ER' + row + ',ES4*ES' + row + ',ET4*ET' + row + ',EU4*EU' + row + ',EV4*EV' + row + ',EW4*EW' + row + ',EX4*EX' + row + ',EY4*EY' + row + ',EZ4*EZ' + row + ', FA4*FA' + row + ',FB4*FB' + row + ',FC4*FC' + row + ',FD4*FD' + row + ',FE4*FE' + row + ',FF4*FF' + row + ',FG4*FG' + row + ',FH4*FH' + row + ',FI4*FI' + row + ',FJ4*FJ' + row + ',FK4*FK' + row + ',FL4*FL' + row + ',FM4*FM' + row + ',FN4*FN' + row + ',FO4*FO' + row + ',FP4*FP' + row + ',FQ4*FQ' + row + ',FR4*FR' + row + ',FS4*FS' + row + ',FT4*FT' + row + ',FU4*FU' + row + ',FV4*FV' + row + ',FW4*FW' + row + ',FX4*FX' + row + ',FY4*FY' + row + ',FZ4*FZ' + row + ',GA4*GA' + row + ',GB4*GB' + row + ',GC4*GC' + row + ',GD4*GD' + row + ',GE4*GE' + row + ',GF4*GF' + row + ',GG4*GG' + row + ',GH4*GH' + row + ',GI4*GI' + row + ',GJ4*GJ' + row + ',GK4*GK' + row + ',GL4*GL' + row + ',GM4*GM' + row + ',GN4*GN' + row + ',GO4*GO' + row + ',GP4*GP' + row + ',GQ4*GQ' + row + ',GR4*GR' + row + ',GS4*GS' + row + ',GT4*GT' + row + ',GU4*GU' + row + ',GV4*GV' + row + ',GW4*GW' + row + ',GX4*GX' + row + ',GY4*GY' + row + ',GZ4*GZ' + row + ',HA4*HA' + row + ',HB4*HB' + row + ',HC4*HC' + row + ',HD4*HD' + row + ',HE4*HE' + row + ')'
   // Deal with first 5 Columns separately
-  var formula = '=SUM(V4*V' + row + ',W4*W' + row + ',X4*X' + row + ',Y4*Y' + row + ',Z4*Z' + row
+  var formula = '=SUM($V$4*V' + row + ',$W$4*W' + row + ',$X$4*X' + row + ',$Y$4*Y' + row + ',$Z$4*Z' + row
   var firstLetter
   var secondLetter
   var char1
@@ -88,14 +88,14 @@ var totalPointsFormula = function (row) {
     for (secondLetter = 0; secondLetter < 26; secondLetter++) {
       char1 = String.fromCharCode(firstLetter + 65)
       char2 = String.fromCharCode(secondLetter + 65)
-      formula = formula + ',' + char1 + char2 + 4 + '*' + char1 + char2 + row
+      formula = formula + ',$' + char1 + char2 + '$' + 4 + '*' + char1 + char2 + row
     }
   }
   // Deal with last 5 columns in last separate loop
   for (secondLetter = 0; secondLetter < 5; secondLetter++) {
     char1 = 'H'
     char2 = String.fromCharCode(secondLetter + 65)
-    formula = formula + ',' + char1 + char2 + 4 + '*' + char1 + char2 + row
+    formula = formula + ',$' + char1 + char2 + '$' + 4 + '*' + char1 + char2 + row
   }
   // Close Formula
   formula = formula + ')'
@@ -107,49 +107,49 @@ var totalCasesFormula = function (row) {
 }
 
 // Add formulas to Cells C to U (Data below "Total Cases" Column to "Current % Capacity" Column in produced spreadsheet)
-var inputFormulas = function (ws, row) {
-  ws.cell(row, 3).formula(totalCasesFormula(row)) // Total Cases
-  ws.cell(row, 9).formula('=IFERROR((H' + row + '/R' + row + '),0)').style(this.percentageStyle) // CMS %
-  ws.cell(row, 11).formula('=IFERROR((J' + row + '/R' + row + '),0)').style(this.percentageStyle) // GS %
-  ws.cell(row, 12).formula('=HF' + row + '*HF4') // SDR Points
-  ws.cell(row, 13).formula('=HG' + row + '*HG4') // FDR Points
-  ws.cell(row, 14).formula('=HH' + row + '*HH4') // Parom Points
-  ws.cell(row, 15).formula('=HI' + row + '*HI4') // ARMS Comm Points
-  ws.cell(row, 16).formula('=HJ' + row + '*HJ4') // ARMS Licence Points
-  ws.cell(row, 17).formula(totalPointsFormula(row)) // Total Caseload Points
-  ws.cell(row, 18).formula('=SUM(H' + row + ',J' + row + ',L' + row + ':Q' + row + ')') // Overall Total Points
-  ws.cell(row, 19).formula('=IFERROR(ROUNDDOWN(((D' + row + ' * (E' + row + '/F' + row + '))*((E' + row + '-G' + row + ')/E' + row + ')),0),0)').style(this.roundedStyle) // Available Points
-  ws.cell(row, 20).formula('=S' + row + '-R' + row).style(this.roundedStyle) // Remaining Points
-  ws.cell(row, 21).formula('=IFERROR(R' + row + '/S' + row + ',0)').style(this.percentageStyle) // Current % Capacity
+var inputMainBodyFormulas = function (ws, row) {
+  ws.cell(row, 3).formula(totalCasesFormula(row)).style(styles.caseStyle) // Total Cases
+  ws.cell(row, 9).formula('=IFERROR((H' + row + '/R' + row + '),0)').style(this.styles.percentageStyle) // CMS %
+  ws.cell(row, 11).formula('=IFERROR((J' + row + '/R' + row + '),0)').style(this.styles.percentageStyle) // GS %
+  ws.cell(row, 12).formula('=HF' + row + '*HF4').style(styles.caseStyle) // SDR Points
+  ws.cell(row, 13).formula('=HG' + row + '*HG4').style(styles.caseStyle) // FDR Points
+  ws.cell(row, 14).formula('=HH' + row + '*HH4').style(styles.caseStyle) // Parom Points
+  ws.cell(row, 15).formula('=HI' + row + '*HI4').style(styles.caseStyle) // ARMS Comm Points
+  ws.cell(row, 16).formula('=HJ' + row + '*HJ4').style(styles.caseStyle) // ARMS Licence Points
+  ws.cell(row, 17).formula(totalPointsFormula(row)).style(styles.caseStyle) // Total Caseload Points
+  ws.cell(row, 18).formula('=SUM(H' + row + ',J' + row + ',L' + row + ':Q' + row + ')').style(styles.caseStyle) // Overall Total Points
+  ws.cell(row, 19).formula('=IFERROR(ROUNDDOWN(((D' + row + ' * (E' + row + '/F' + row + '))*((E' + row + '-G' + row + ')/E' + row + ')),0),0)').style(this.styles.roundedStyle) // Available Points
+  ws.cell(row, 20).formula('=S' + row + '-R' + row).style(this.styles.roundedStyle) // Remaining Points
+  ws.cell(row, 21).formula('=IFERROR(R' + row + '/S' + row + ',0)').style(this.styles.percentageStyle) // Current % Capacity
 }
 
-var inputTotals = function (ws, row) {
+var inputBottomTotals = function (ws, row) {
   var dataEndRow = row - 1
-  ws.cell(row, 1).string('Total / Average')
-  ws.cell(row, 3).formula('=SUM(C' + 5 + ':C' + dataEndRow + ')').style(this.sumStyle)
-  ws.cell(row, 4).formula('=AVERAGE(D' + 5 + ':D' + dataEndRow + ')').style(this.averageStyle)
-  ws.cell(row, 5).formula('=SUM(E' + 5 + ':E' + dataEndRow + ')').style(this.sumStyle)
-  ws.cell(row, 6).formula('=SUM(F' + 5 + ':F' + dataEndRow + ')').style(this.sumStyle)
-  ws.cell(row, 7).formula('=SUM(G' + 5 + ':G' + dataEndRow + ')').style(this.sumStyle)
-  ws.cell(row, 8).formula('=SUM(H' + 5 + ':H' + dataEndRow + ')').style(this.sumStyle)
-  ws.cell(row, 9).formula('=IFERROR((H' + row + '/R' + row + '),0)').style(this.cmsGsPercentageStyle)
-  ws.cell(row, 10).formula('=SUM(J' + 5 + ':J' + dataEndRow + ')').style(this.sumStyle)
-  ws.cell(row, 11).formula('=IFERROR((J' + row + '/R' + row + '),0)').style(this.cmsGsPercentageStyle)
-  ws.cell(row, 12).formula('=SUM(L' + 5 + ':L' + dataEndRow + ')').style(this.sumStyle)
-  ws.cell(row, 13).formula('=SUM(M' + 5 + ':M' + dataEndRow + ')').style(this.sumStyle)
-  ws.cell(row, 14).formula('=SUM(N' + 5 + ':N' + dataEndRow + ')').style(this.sumStyle)
-  ws.cell(row, 15).formula('=SUM(O' + 5 + ':O' + dataEndRow + ')').style(this.sumStyle)
-  ws.cell(row, 16).formula('=SUM(P' + 5 + ':P' + dataEndRow + ')').style(this.sumStyle)
-  ws.cell(row, 17).formula('=SUM(Q' + 5 + ':Q' + dataEndRow + ')').style(this.sumStyle)
-  ws.cell(row, 18).formula('=SUM(R' + 5 + ':R' + dataEndRow + ')').style(this.sumStyle)
-  ws.cell(row, 19).formula('=SUM(S' + 5 + ':S' + dataEndRow + ')').style(this.roundedSumStyle)
-  ws.cell(row, 20).formula('=SUM(T' + 5 + ':T' + dataEndRow + ')').style(this.roundedSumStyle)
-  ws.cell(row, 21).formula('=IFERROR(R' + row + '/S' + row + ',0)').style(this.averagePercentageStyle)
-  ws.cell(row, 22).formula('=SUM(V' + 5 + ':V' + dataEndRow + ')').style(this.sumStyle)
-  ws.cell(row, 23).formula('=SUM(W' + 5 + ':W' + dataEndRow + ')').style(this.sumStyle)
-  ws.cell(row, 24).formula('=SUM(X' + 5 + ':X' + dataEndRow + ')').style(this.sumStyle)
-  ws.cell(row, 25).formula('=SUM(Y' + 5 + ':Y' + dataEndRow + ')').style(this.sumStyle)
-  ws.cell(row, 26).formula('=SUM(Z' + 5 + ':Z' + dataEndRow + ')').style(this.sumStyle)
+  ws.cell(row, 1).string('Total / Average').style(this.styles.caseStyle)
+  ws.cell(row, 3).formula('=SUM($C$' + 5 + ':C' + dataEndRow + ')').style(this.styles.sumStyle)
+  ws.cell(row, 4).formula('=AVERAGE($D$' + 5 + ':D' + dataEndRow + ')').style(this.styles.averageStyle)
+  ws.cell(row, 5).formula('=SUM($E$' + 5 + ':E' + dataEndRow + ')').style(this.styles.sumStyle)
+  ws.cell(row, 6).formula('=SUM($F$' + 5 + ':F' + dataEndRow + ')').style(this.styles.sumStyle)
+  ws.cell(row, 7).formula('=SUM($G$' + 5 + ':G' + dataEndRow + ')').style(this.styles.sumStyle)
+  ws.cell(row, 8).formula('=SUM($H$' + 5 + ':H' + dataEndRow + ')').style(this.styles.sumStyle)
+  ws.cell(row, 9).formula('=IFERROR((H' + row + '/R' + row + '),0)').style(this.styles.cmsGsPercentageStyle)
+  ws.cell(row, 10).formula('=SUM($J$' + 5 + ':J' + dataEndRow + ')').style(this.styles.sumStyle)
+  ws.cell(row, 11).formula('=IFERROR((J' + row + '/R' + row + '),0)').style(this.styles.cmsGsPercentageStyle)
+  ws.cell(row, 12).formula('=SUM($L$' + 5 + ':L' + dataEndRow + ')').style(this.styles.sumStyle)
+  ws.cell(row, 13).formula('=SUM($M$' + 5 + ':M' + dataEndRow + ')').style(this.styles.sumStyle)
+  ws.cell(row, 14).formula('=SUM($N$' + 5 + ':N' + dataEndRow + ')').style(this.styles.sumStyle)
+  ws.cell(row, 15).formula('=SUM($O$' + 5 + ':O' + dataEndRow + ')').style(this.styles.sumStyle)
+  ws.cell(row, 16).formula('=SUM($P$' + 5 + ':P' + dataEndRow + ')').style(this.styles.sumStyle)
+  ws.cell(row, 17).formula('=SUM($Q$' + 5 + ':Q' + dataEndRow + ')').style(this.styles.sumStyle)
+  ws.cell(row, 18).formula('=SUM($R$' + 5 + ':R' + dataEndRow + ')').style(this.styles.sumStyle)
+  ws.cell(row, 19).formula('=SUM($S$' + 5 + ':S' + dataEndRow + ')').style(this.styles.roundedSumStyle)
+  ws.cell(row, 20).formula('=SUM($T$' + 5 + ':T' + dataEndRow + ')').style(this.styles.roundedSumStyle)
+  ws.cell(row, 21).formula('=IFERROR(R' + row + '/S' + row + ',0)').style(this.styles.averagePercentageStyle)
+  ws.cell(row, 22).formula('=SUM($V$' + 5 + ':V' + dataEndRow + ')').style(this.styles.sumStyle)
+  ws.cell(row, 23).formula('=SUM($W$' + 5 + ':W' + dataEndRow + ')').style(this.styles.sumStyle)
+  ws.cell(row, 24).formula('=SUM($X$' + 5 + ':X' + dataEndRow + ')').style(this.styles.sumStyle)
+  ws.cell(row, 25).formula('=SUM($Y$' + 5 + ':Y' + dataEndRow + ')').style(this.styles.sumStyle)
+  ws.cell(row, 26).formula('=SUM($Z$' + 5 + ':Z' + dataEndRow + ')').style(this.styles.sumStyle)
 
   var columnNo = 27
   var firstLetter
@@ -160,39 +160,33 @@ var inputTotals = function (ws, row) {
     for (secondLetter = 0; secondLetter < 26; secondLetter++) {
       char1 = String.fromCharCode(firstLetter + 65)
       char2 = String.fromCharCode(secondLetter + 65)
-      ws.cell(row, columnNo).formula('=SUM(' + char1 + char2 + 5 + ':' + char1 + char2 + dataEndRow + ')').style(this.sumStyle)
+      ws.cell(row, columnNo).formula('=SUM(' + char1 + char2 + 5 + ':' + char1 + char2 + dataEndRow + ')').style(this.styles.sumStyle)
       columnNo = columnNo + 1
     }
   }
   for (secondLetter = 0; secondLetter < 10; secondLetter++) {
     char1 = 'H'
     char2 = String.fromCharCode(secondLetter + 65)
-    ws.cell(row, columnNo).formula('=SUM(' + char1 + char2 + 5 + ':' + char1 + char2 + dataEndRow + ')').style(this.sumStyle)
+    ws.cell(row, columnNo).formula('=SUM(' + char1 + char2 + 5 + ':' + char1 + char2 + dataEndRow + ')').style(this.styles.sumStyle)
     columnNo = columnNo + 1
   }
 }
 
 var setTierTotals = function (ws, rowStart, columnStart, casesForThisTier, t2a) {
   if (t2a) {
-    ws.cell(rowStart, columnStart).number(casesForThisTier.t2aTotalCases)
-    ws.cell(rowStart, columnStart + 1).number(casesForThisTier.t2aWarrantsTotal)
-    ws.cell(rowStart, columnStart + 2).number(casesForThisTier.t2aUPW)
-    ws.cell(rowStart, columnStart + 3).number(casesForThisTier.t2aOverdueTerminationsTotal)
+    ws.cell(rowStart, columnStart).number(casesForThisTier.t2aTotalCases).style(styles.caseStyle)
+    ws.cell(rowStart, columnStart + 1).number(casesForThisTier.t2aWarrantsTotal).style(styles.caseStyle)
+    ws.cell(rowStart, columnStart + 2).number(casesForThisTier.t2aUPW).style(styles.caseStyle)
+    ws.cell(rowStart, columnStart + 3).number(casesForThisTier.t2aOverdueTerminationsTotal).style(styles.caseStyle)
   } else {
-    ws.cell(rowStart, columnStart).number(casesForThisTier.totalCases)
-    ws.cell(rowStart, columnStart + 1).number(casesForThisTier.warrantsTotal)
-    ws.cell(rowStart, columnStart + 2).number(casesForThisTier.UPW)
-    ws.cell(rowStart, columnStart + 3).number(casesForThisTier.overdueTerminationsTotal)
+    ws.cell(rowStart, columnStart).number(casesForThisTier.totalCases).style(styles.caseStyle)
+    ws.cell(rowStart, columnStart + 1).number(casesForThisTier.warrantsTotal).style(styles.caseStyle)
+    ws.cell(rowStart, columnStart + 2).number(casesForThisTier.UPW).style(styles.caseStyle)
+    ws.cell(rowStart, columnStart + 3).number(casesForThisTier.overdueTerminationsTotal).style(styles.caseStyle)
   }
 }
 
-module.exports = function (ws, scenarioData, typeTierGroupLength, tiersPerType, sumStyle, averageStyle, averagePercentageStyle, percentageStyle, roundedStyle, roundedSumStyle, cmsGsPercentageStyle) {
-  this.sumStyle = sumStyle
-  this.averageStyle = averageStyle
-  this.averagePercentageStyle = averagePercentageStyle
-  this.percentageStyle = percentageStyle
-  this.roundedStyle = roundedStyle
-  this.roundedSumStyle = roundedSumStyle
-  this.cmsGsPercentageStyle = cmsGsPercentageStyle
+module.exports = function (ws, scenarioData, typeTierGroupLength, tiersPerType, styles) {
+  this.styles = styles
   inputCaseData(ws, scenarioData, typeTierGroupLength, tiersPerType)
 }
--- app/services/get-export-xlsx.js
+++ app/services/get-export-xlsx.js
@@ -4,6 +4,8 @@ const nameHeaders = require('../constants/excel-headings').nameHeaders
 const reportHeaders = require('../constants/excel-headings').reportHeaders
 const caseTypeHeaders = require('../constants/excel-headings').caseTypeHeaders
 const inputScenarioCaseData = require('./excel/input-scenario-case-data')
+const createStyles = require('./helpers/create-styles')
+const determineStyles = require('./helpers/determine-styles')
 const armsCommMultiplier = 4
 const armsLicMultiplier = 2
 const typeTierGroupLength = 4 // the number of fields for each tier and type of case
@@ -11,110 +13,40 @@ const tiersPerType = 8
 
 module.exports = function (caseData, t2aCaseData, scenarioData) {
   var wb = new excel.Workbook()
-  var ws = wb.addWorksheet('Sheet 1')
-  var caseStyle = wb.createStyle({
-    alignment: {
-      horizontal: 'center'
-    },
-    font: {
-      size: 12
+  var ws = wb.addWorksheet('Sheet 1', {
+    'sheetFormat': {
+      'defaultColWidth': 8,
     }
   })
+  var styles = createStyles(wb)
 
-  var sumStyle = wb.createStyle({
-    font: {
-      size: 12,
-      color: '#FFFFFF'
-    },
-    fill: {
-      type: 'pattern',
-      patternType: 'solid',
-      fgColor: '#0000FF'
-    }
-  })
-
-  var roundedSumStyle = wb.createStyle({
-    font: {
-      size: 12,
-      color: '#FFFFFF'
-    },
-    fill: {
-      type: 'pattern',
-      patternType: 'solid',
-      fgColor: '#0000FF'
-    },
-    numberFormat: '#; -#; 0'
-  })
-
-  var averageStyle = wb.createStyle({
-    font: {
-      size: 12
-    },
-    fill: {
-      type: 'pattern',
-      patternType: 'solid',
-      fgColor: '#FFFF00'
-    }
-  })
-
-  var averagePercentageStyle = wb.createStyle({
-    font: {
-      size: 12
-    },
-    numberFormat: '#%; -#%; 0%',
-    fill: {
-      type: 'pattern',
-      patternType: 'solid',
-      fgColor: '#FFFF00'
-    }
-  })
-
-  var cmsGsPercentageStyle = wb.createStyle({
-    font: {
-      size: 12
-    },
-    numberFormat: '0.0%; -0.0%; 0%',
-    fill: {
-      type: 'pattern',
-      patternType: 'solid',
-      fgColor: '#FFFF00'
-    }
-  })
-
-  var percentageStyle = wb.createStyle({
-    font: {
-      size: 12
-    },
-    numberFormat: '#%; -#%; 0%'
-  })
-
-  var roundedStyle = wb.createStyle({
-    font: {
-      size: 12
-    },
-    numberFormat: '#; -#; 0'
-  })
-
-  mergeCells(ws, caseStyle)
+  mergeCells(ws, styles.caseStyle)
   var start = 22
-  setCaseHeaders(ws, start, caseStyle)
+  var additionalHeading = ''
+  setCaseHeaders(ws, start, styles, additionalHeading)
   start = 118
-  setCaseHeaders(ws, start, caseStyle)
-  setReportHeaders(ws, caseStyle)
-  setHeaders(ws)
-  setCaseTypeHeaders(ws, caseStyle)
-  setTierWeightings(ws, caseStyle, caseData)
-  setTierWeightings(ws, caseStyle, t2aCaseData)
-  setReportWeightings(ws, caseStyle, caseData)
-  inputScenarioCaseData(ws, scenarioData, typeTierGroupLength, tiersPerType, sumStyle, averageStyle, averagePercentageStyle, percentageStyle, roundedStyle, roundedSumStyle, cmsGsPercentageStyle)
+  additionalHeading = 'T2A '
+  setCaseHeaders(ws, start, styles, additionalHeading)
+  setReportHeaders(ws, styles)
+  setHeaders(ws, styles.nameHeadersStyle)
+  setCaseTypeHeaders(ws, styles)
+  setTierWeightings(ws, styles, caseData)
+  setTierWeightings(ws, styles, t2aCaseData)
+  setReportWeightings(ws, styles, caseData)
+  inputScenarioCaseData(ws, scenarioData, typeTierGroupLength, tiersPerType, styles)
+  ws.row(4).freeze()
+  ws.column(21).freeze()
+  setRowHeights(ws)
   return wb
 }
 
-var setReportHeaders = function (ws, caseStyle) {
+var setReportHeaders = function (ws, styles) {
   var start = 214
   var count = 0
   while (count < reportHeaders.length) {
-    ws.cell(3, start).string(reportHeaders[count]).style(caseStyle)
+    var styleToApply = determineStyles.determineWeightingStyle(start, styles)
+    ws.cell(2, start).style(styleToApply)
+    ws.cell(3, start).string(reportHeaders[count]).style(styleToApply)
     count = count + 1
     start = start + 1
   }
@@ -126,35 +58,45 @@ var mergeCells = function (ws, caseStyle) {
   ws.cell(1, 214, 1, 218, true).string('Reports').style(caseStyle)
 }
 
-var setCaseHeaders = function (ws, start, caseStyle) {
+var setCaseHeaders = function (ws, start, styles, additionalHeading) {
   var count = 0
   while (count < caseHeaders.length) {
-    ws.cell(2, start, 2, start + 3, true).string(caseHeaders[count]).style(caseStyle)
+    var completeHeader = additionalHeading + caseHeaders[count]
+    var styleToApply = determineStyles.determineCaseStyle(completeHeader, styles)
+    ws.cell(2, start, 2, start + 3, true).string(completeHeader).style(styleToApply)
     count = count + 1
     start = start + typeTierGroupLength
   }
 }
 
-var setHeaders = function (ws) {
+var setHeaders = function (ws, nameHeadersStyle) {
   var i
+  for (i = 0; i < nameHeaders.length; i++) {
+    ws.cell(2, i + 1)
+    .style(nameHeadersStyle)
+    ws.cell(4, i + 1)
+    .style(nameHeadersStyle)
+  }
   for (i = 0; i < nameHeaders.length; i++) {
     ws.cell(3, i + 1)
     .string(nameHeaders[i])
+    .style(nameHeadersStyle)
   }
 }
 
-var setCaseTypeHeaders = function (ws, caseStyle) {
+var setCaseTypeHeaders = function (ws, styles) {
   var count = 0
   var i
   for (i = 22; i < 214; i = i + typeTierGroupLength) {
-    ws.cell(3, i).string(caseTypeHeaders[count]).style(caseStyle)
-    ws.cell(3, i + 1).string(caseTypeHeaders[count + 1]).style(caseStyle)
-    ws.cell(3, i + 2).string(caseTypeHeaders[count + 2]).style(caseStyle)
-    ws.cell(3, i + 3).string(caseTypeHeaders[count + 3]).style(caseStyle)
+    var styleToApply = determineStyles.determineWeightingStyle(i, styles)
+    ws.cell(3, i).string(caseTypeHeaders[count]).style(styleToApply)
+    ws.cell(3, i + 1).string(caseTypeHeaders[count + 1]).style(styleToApply)
+    ws.cell(3, i + 2).string(caseTypeHeaders[count + 2]).style(styleToApply)
+    ws.cell(3, i + 3).string(caseTypeHeaders[count + 3]).style(styleToApply)
   }
 }
 
-var setTierWeightings = function (ws, caseStyle, points) {
+var setTierWeightings = function (ws, styles, points) {
   var keys = Object.keys(points)
   var start
   if (points['isT2A']) {
@@ -165,18 +107,19 @@ var setTierWeightings = function (ws, caseStyle, points) {
   var count = 0
   var i
   for (i = 0; i < 24; i++) {
+    var styleToApply = determineStyles.determineWeightingStyle(start, styles)
     switch (i % 8) {
       case 0:
-        ws.cell(4, start).number(0)
-        ws.cell(4, start + 1).number(0)
-        ws.cell(4, start + 2).number(0)
-        ws.cell(4, start + 3).number(0)
+        ws.cell(4, start).number(0).style(styleToApply)
+        ws.cell(4, start + 1).number(0).style(styleToApply)
+        ws.cell(4, start + 2).number(0).style(styleToApply)
+        ws.cell(4, start + 3).number(0).style(styleToApply)
         break
       default:
-        ws.cell(4, start).number(points[keys[count]])
-        ws.cell(4, start + 1).number(0 - points[keys[count]])
-        ws.cell(4, start + 2).number(0)
-        ws.cell(4, start + 3).number(0 - points[keys[count]])
+        ws.cell(4, start).number(points[keys[count]]).style(styleToApply)
+        ws.cell(4, start + 1).number(0 - points[keys[count]]).style(styleToApply)
+        ws.cell(4, start + 2).number(0).style(styleToApply)
+        ws.cell(4, start + 3).number(0 - points[keys[count]]).style(styleToApply)
         count = count + 1
         break
     }
@@ -184,10 +127,15 @@ var setTierWeightings = function (ws, caseStyle, points) {
   }
 }
 
-var setReportWeightings = function (ws, caseStyle, points) {
-  ws.cell(4, 214).number(points['sdr'])
-  ws.cell(4, 215).number(points['sdrConversion'])
-  ws.cell(4, 216).number(points['parom'])
-  ws.cell(4, 217).number(points['weightingArmsCommunity'] * armsCommMultiplier)
-  ws.cell(4, 218).number(points['weightingArmsLicense'] * armsLicMultiplier)
+var setReportWeightings = function (ws, styles, points) {
+  var styleToApply = determineStyles.determineWeightingStyle(214, styles)
+  ws.cell(4, 214).number(points['sdr']).style(styleToApply)
+  ws.cell(4, 215).number(points['sdrConversion']).style(styleToApply)
+  ws.cell(4, 216).number(points['parom']).style(styleToApply)
+  ws.cell(4, 217).number(points['weightingArmsCommunity'] * armsCommMultiplier).style(styleToApply)
+  ws.cell(4, 218).number(points['weightingArmsLicense'] * armsLicMultiplier).style(styleToApply)
+}
+
+var setRowHeights = function (ws) {
+  ws.row(3).setHeight(75)
 }
