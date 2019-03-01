1b9dac09c2feee123b72cbb2e442bd7530063d88
kevinfox <kevinfox@kainos.com>
2019-03-01 19:55:10 +0000

    WMT0116: Fixing caseload average percentages being calculated incorrectly

--- app/services/get-capacity-view.js
+++ app/services/get-capacity-view.js
@@ -35,7 +35,7 @@ module.exports = function (id, capacityDateRange, organisationLevel) {
 
 var parseCapacityBreakdown = function (workloadReports, organisationLevel) {
   var capacityBreakdown = []
-  var totals = { name: 'Total / Average', capacity: 0, totalCases: 0, totalARMS: 0, totalGs: 0, totalCMS: 0, totalSDRs: 0, totalParoms: 0, totalSdrConversions: 0, totalTotalT2aCases: 0, totalCMSPoints: 0, totalGSPoints: 0, totalPoints: 0, availablePoints: 0}
+  var totals = {name: 'Total / Average', capacity: 0, totalCases: 0, totalARMS: 0, totalGs: 0, totalCMS: 0, totalSDRs: 0, totalParoms: 0, totalSdrConversions: 0, totalTotalT2aCases: 0, totalCMSPoints: 0, totalGSPoints: 0, totalPoints: 0, availablePoints: 0}
   var totalNumberOfGrades = 0
 
   if (organisationLevel === organisationConstant.TEAM.name) {
