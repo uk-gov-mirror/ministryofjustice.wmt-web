18f3a88e5987209d5a2d62898a551ea450254248
kevinfox <kevinfox@kainos.com>
2019-03-06 13:33:17 +0000

    WMT0093: Fixing failing integration tests that were failing due to being ran after db migrations

---
+++ app/services/data/get-region-by-name.js
@@ -0,0 +1,14 @@
+const knex = require('../../../knex').web
+
+module.exports = function (name) {
+  if (name === undefined) {
+    throw new ReferenceError('Region Name is not defined')
+  }
+
+  return knex('region')
+    .where('description', name)
+    .first('id')
+    .then(function (region) {
+      return region['id']
+    })
+}
