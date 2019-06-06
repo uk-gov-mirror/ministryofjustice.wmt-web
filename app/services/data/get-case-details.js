1eb5711ccc11995c8118147b32fe9af86cb40619
kevinfox <kevinfox@kainos.com>
2019-06-06 18:12:25 +0100

    WMT0115: Added CRNs to CMS/GS exports. Added Suspended Lifer data to the capacity tab at offender manager level

--- app/services/data/get-case-details.js
+++ app/services/data/get-case-details.js
@@ -5,10 +5,46 @@ module.exports = function (id) {
     'tierCode',
     'rowType',
     'caseReferenceNo',
-    'caseType'
+    'caseType',
+    'inCustody',
+    'registerLevel',
+    'registerCategory',
+    'registerCategoryDescription',
+    'registrationDate'
   ]
 
   return knex('case_details_view')
     .columns(selectList)
     .where('workloadOwnerId', id)
+    .then(function (results) {
+      if (results.length > 0) {
+        results.forEach(function (result) {
+          formatSuspendedLiferFields(result)
+        })
+      }
+      return results
+    })
+}
+
+var formatSuspendedLiferFields = function (result) {
+  if (result.rowType !== 'Suspended Lifer') {
+    result.inCustody = 'N/A'
+    result.registerLevel = 'N/A'
+    result.registerCategory = 'N/A'
+    result.registerCategoryDescription = 'N/A'
+    result.registrationDate = 'N/A'
+  } else {
+    if (result.inCustody === 'N') {
+      result.inCustody = 'No'
+    } else if (result.inCustody === 'Y') {
+      result.inCustody = 'Yes'
+    }
+    if (result.registrationDate) {
+      var newDate = new Date(result.registrationDate)
+      var year = newDate.getFullYear()
+      var month = newDate.getMonth() + 1
+      var dt = newDate.getDate()
+      result.registrationDate = dt + '-' + month + '-' + year
+    }
+  }
 }
--- app/services/get-export-csv.js
+++ app/services/get-export-csv.js
@@ -27,10 +27,10 @@ const ARMS_EXPORT_FIELD_NAMES = ['Region Name', 'LDU Cluster', 'Team Name', 'Ass
 const ARMS_EXPORT_FIELDS = ['regionName', 'lduName', 'teamName', 'assessmentDate', 'CRN', 'omName', 'omGrade', 'sentencetype', 'releaseDate']
 const CASE_DETAILS_EXPORT_FIELD_NAMES = ['Region Name', 'LDU Cluster', 'Team Name', 'Tier Code', 'Row Type', 'CRN', 'Case Type', 'Offender Manager Name', 'Grade Code']
 const CASE_DETAILS_EXPORT_FIELDS = ['regionName', 'lduName', 'teamName', 'tierCode', 'rowType', 'caseReferenceNo', 'caseType', 'offenderManagerName', 'gradeCode']
-const GROUP_SUPERVISION_EXPORT_FIELD_NAMES = ['Region Name', 'LDU Cluster', 'Team Name', 'Contact Date', 'CRN', 'Offender Manager Name', 'Offender Manager Grade', 'Contact Type Description', 'Contact Code', 'Points']
-const GROUP_SUPERVISION_EXPORT_FIELDS = ['regionName', 'lduName', 'teamName', 'contactDate', 'CRN', 'omName', 'omGradeCode', 'contactDescription', 'contactCode', 'points']
-const CMS_EXPORT_FIELD_NAMES = ['Contact Region Name', 'Contact LDU Cluster', 'Contact Team Name', 'Contact Date', 'Contact Name', 'Contact Grade', 'OM Region Name', 'OM LDU Cluster', 'OM Team Name', 'CRN', 'OM Name', 'OM Grade', 'Contact Type Description', 'Contact Code', 'Contact Points', 'OM Points']
-const CMS_EXPORT_FIELDS = ['contactRegionName', 'contactLduName', 'contactTeamName', 'contactDate', 'contactName', 'contactGradeCode', 'omRegionName', 'omLduName', 'omTeamName', 'contactId', 'omName', 'omGradeCode', 'contactDescription', 'contactCode', 'contactPoints', 'omPoints']
+const GROUP_SUPERVISION_EXPORT_FIELD_NAMES = ['Region Name', 'LDU Cluster', 'Team Name', 'Contact Date', 'CRN', 'Contact ID,', 'Offender Manager Name', 'Offender Manager Grade', 'Contact Type Description', 'Contact Code', 'Points']
+const GROUP_SUPERVISION_EXPORT_FIELDS = ['regionName', 'lduName', 'teamName', 'contactDate', 'caseRefNo', 'contactId', 'omName', 'omGradeCode', 'contactDescription', 'contactCode', 'points']
+const CMS_EXPORT_FIELD_NAMES = ['Contact Region Name', 'Contact LDU Cluster', 'Contact Team Name', 'Contact Date', 'Contact Name', 'Contact Grade', 'OM Region Name', 'OM LDU Cluster', 'OM Team Name', 'CRN', 'Contact Id', 'OM Name', 'OM Grade', 'Contact Type Description', 'Contact Code', 'Contact Points', 'OM Points']
+const CMS_EXPORT_FIELDS = ['contactRegionName', 'contactLduName', 'contactTeamName', 'contactDate', 'contactName', 'contactGradeCode', 'omRegionName', 'omLduName', 'omTeamName', 'caseRefNo', 'contactId', 'omName', 'omGradeCode', 'contactDescription', 'contactCode', 'contactPoints', 'omPoints']
 
 module.exports = function (organisationLevel, result, tab) {
   var filename
