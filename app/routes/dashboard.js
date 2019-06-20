28b7d55b92305522b659794374440d84ebee33c5
kevinfox <kevinfox@kainos.com>
2019-06-20 10:15:38 +0100

    WMT0123: Removing functionality now moved to the worker

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,6 +1,5 @@
 const authorisation = require('../authorisation')
 const Unauthorized = require('../services/errors/authentication-error').Unauthorized
-const dashboard = require('../services/dashboard')
 
 module.exports = function (router) {
   router.get('/dashboard', function (req, res, next) {
@@ -11,6 +10,5 @@ module.exports = function (router) {
         return res.status(error.statusCode).redirect(error.redirect)
       }
     }
-    return dashboard()
   })
 }
