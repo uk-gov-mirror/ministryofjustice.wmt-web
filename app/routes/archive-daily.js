57c1a68085fe62c26c832a5b7485d3002d985c50
kevinfox <kevinfox@kainos.com>
2019-06-25 16:01:00 +0100

    WMT0123: Adding changes from WMT0134

--- app/routes/archive-daily.js
+++ app/routes/archive-daily.js
@@ -16,6 +16,7 @@ const title = 'Archived Daily Caseload Data'
 const newDataStartDay = require('../../config').NEW_DATABASE_START_DAY
 const newDataStartMonth = require('../../config').NEW_DATABASE_START_MONTH
 const newDataStartYear = require('../../config').NEW_DATABASE_START_YEAR
+const heDecode = require('he')
 
 const newDataStartDate = dateFormatter.build(newDataStartDay, newDataStartMonth, newDataStartYear)
 
@@ -70,7 +71,7 @@ module.exports = function (router) {
     }
 
     var authorisedUserRole = authorisation.getAuthorisedUserRole(req)
-    var extraCriteria = req.body['daily-multi-search-field-entry']
+    var extraCriteria = heDecode.decode(req.body['daily-multi-search-field-entry'])
     if (errors) {
       return renderResults(viewTemplate, title, res, errors, null, authorisedUserRole, archiveDateRange, extraCriteria)
     }
@@ -117,7 +118,7 @@ module.exports = function (router) {
     }
 
     var authorisedUserRole = authorisation.getAuthorisedUserRole(req)
-    var extraCriteria = req.body['daily-multi-search-field-entry']
+    var extraCriteria = heDecode.decode(req.body['daily-multi-search-field-entry'])
 
     if (errors) {
       return renderResults(viewTemplate, title, res, errors, null, authorisedUserRole, archiveDateRange, extraCriteria)
