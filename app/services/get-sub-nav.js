0f3536b0717fd77f681c16a5c0baf7b4a96fe044
kevinfox <kevinfox@kainos.com>
2019-06-25 15:30:53 +0100

    WMT0123: Adding web changes to allow managers to donwload the dashboard

--- app/services/get-sub-nav.js
+++ app/services/get-sub-nav.js
@@ -3,7 +3,7 @@ const linkGenerator = require('./helpers/link-generator')
 const organisationUnitConstants = require('../constants/organisation-unit')
 const workloadConstants = require('../constants/workload-type')
 
-module.exports = function (id, organisationalUnitName, currentPath, workloadType = workloadConstants.PROBATION) {
+module.exports = function (id, organisationalUnitName, currentPath, workloadType = workloadConstants.PROBATION, authorisation, userRole) {
   var baseLink = linkGenerator.fromIdAndNameAndWorkloadType(id, organisationalUnitName, workloadType)
   var navigation = []
 
@@ -19,23 +19,27 @@ module.exports = function (id, organisationalUnitName, currentPath, workloadType
       }
       break
 
-    case workloadConstants.PROBATION:
-      if (isOffenderManager) {
-        navigation.push(new Link('Overview', baseLink + '/overview'))
-        navigation.push(new Link('Capacity', baseLink + '/caseload-capacity'))
-        navigation.push(new Link('Contracted Hours', baseLink + '/contracted-hours'))
-        navigation.push(new Link('Case Progress', baseLink + '/case-progress'))
-        navigation.push(new Link('Reductions', baseLink + '/reductions'))
-      } else {
-        navigation.push(new Link('Overview', baseLink + '/overview'))
-        navigation.push(new Link('Capacity', baseLink + '/caseload-capacity'))
-        navigation.push(new Link('Caseload', baseLink + '/caseload'))
-        navigation.push(new Link('Case Progress', baseLink + '/case-progress'))
-        if (organisationalUnitName !== organisationUnitConstants.NATIONAL.name) {
-          navigation.push(new Link('Export', baseLink + '/export'))
+      case workloadConstants.PROBATION:
+        if (isOffenderManager) {
+          navigation.push(new Link('Overview', baseLink + '/overview'))
+          navigation.push(new Link('Capacity', baseLink + '/caseload-capacity'))
+          navigation.push(new Link('Contracted Hours', baseLink + '/contracted-hours'))
+          navigation.push(new Link('Case Progress', baseLink + '/case-progress'))
+          navigation.push(new Link('Reductions', baseLink + '/reductions'))
+        } else {
+          navigation.push(new Link('Overview', baseLink + '/overview'))
+          navigation.push(new Link('Capacity', baseLink + '/caseload-capacity'))
+          navigation.push(new Link('Caseload', baseLink + '/caseload'))
+          navigation.push(new Link('Case Progress', baseLink + '/case-progress'))
+          if (organisationalUnitName !== organisationUnitConstants.NATIONAL.name) {
+            navigation.push(new Link('Export', baseLink + '/export'))
+          } else {
+            if (authorisation === false || userRole === 'Data Admin' || userRole === 'System Admin' || userRole === 'Manager') {
+              navigation.push(new Link('Dashboard', baseLink + '/dashboard'))
+            }
+          }
         }
-      }
-  }
+    }
 
   navigation[0].active = true
 
