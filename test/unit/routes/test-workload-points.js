c9b9b7c2c27458007457d205bcbb9e7bded5bc71
kevinfox <kevinfox@kainos.com>
2019-08-14 13:47:03 +0100

    WMT0119: Fixing unit tests

--- test/unit/routes/test-workload-points.js
+++ test/unit/routes/test-workload-points.js
@@ -54,7 +54,53 @@ var WORKLOAD_POINTS_TO_POST = {
   weightingArmsCommunity: '15',
   weightingArmsLicense: '10',
   isT2A: 'false',
-  defaultContractedHoursSpo: '0'
+  defaultContractedHoursSpo: '0',
+  adjustment1: '1',
+  adjustment2: '2',
+  adjustment3: '3',
+  adjustment4: '4',
+  adjustment5: '5',
+  adjustment6: '6',
+  adjustment7: '7',
+  adjustment8: '8',
+  adjustment9: '9',
+  adjustment10: '10',
+  adjustment11: '11',
+  adjustment12: '12',
+  adjustment13: '13',
+  adjustment14: '14',
+  adjustment15: '15',
+  adjustment16: '16',
+  adjustment17: '17',
+  adjustment18: '18',
+  adjustment19: '19',
+  adjustment20: '20',
+  adjustment21: '21',
+  adjustment22: '22',
+  adjustment23: '23',
+  adjustment24: '24',
+  adjustment25: '25',
+  adjustment26: '26',
+  adjustment27: '27',
+  adjustment28: '28',
+  adjustment29: '29',
+  adjustment30: '30',
+  adjustment31: '31',
+  adjustment32: '32',
+  adjustment33: '33',
+  adjustment34: '34',
+  adjustment35: '35',
+  adjustment36: '36',
+  adjustment46: '37',
+  adjustment38: '38',
+  adjustment43: '39',
+  adjustment37: '40',
+  adjustment40: '41',
+  adjustment41: '42',
+  adjustment44: '43',
+  adjustment42: '44',
+  adjustment39: '45',
+  adjustment45: '46'
 }
 
 var WORKLOAD_POINTS_T2A_TO_POST = {
@@ -112,8 +158,16 @@ before(function () {
     getWorkloadPoints: sinon.stub().resolves(WORKLOAD_POINTS),
     updateWorkloadPoints: sinon.stub().resolves({})
   }
-  getAdjustmentPointsConfig = sinon.stub.resolves()
-  updateAdjustmentPointsConfig = sinon.stub.resolves()
+  getAdjustmentPointsConfig = sinon.stub().resolves([
+    {
+      adjustmentId: 46,
+      contactCode: 'NGS001',
+      contactDescription: 'GS Rights and Responsibilities session NS',
+      categoryId: 2,
+      points: 1
+    }
+  ])
+  updateAdjustmentPointsConfig = sinon.stub().resolves()
   route = proxyquire('../../../app/routes/workload-points', {
     '../services/workload-points-service': workloadPointsService,
     '../authorisation': authorisationService,
