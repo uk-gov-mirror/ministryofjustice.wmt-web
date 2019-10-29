7c3e24cc77403eefc49161f3795dbf40355db5d0
kevinfox <kevinfox@kainos.com>
2019-10-29 08:29:16 +0000

    WMT0145: Reverting unapproved changes

--- knex.js
+++ knex.js
@@ -1,11 +1,11 @@
 var config = require('./knexfile').web
 var archive = require('./knexfile').archive
 
-// config.pool.afterCreate = function (conn, done) {
-//   conn.batch(['SET ARITHABORT ON'])
-//   .then((err, res) => done(err, conn))
-//   .catch((err, res) => done(err, conn))
-// }
+config.pool.afterCreate = function (conn, done) {
+  conn.batch(['SET ARITHABORT ON'])
+  .then((err, res) => done(err, conn))
+  .catch((err, res) => done(err, conn))
+}
 
 const knexWebSchema = require('knex')(config)
 const knexArchive = require('knex')(archive)
